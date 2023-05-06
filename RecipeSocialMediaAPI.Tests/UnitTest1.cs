27ded8178208ce903c6c5a6680bb735a5b75a05d
Andrei Constantin <ac2042@kent.ac.uk>
2023-05-06 14:00:30 +0100

    Add project files.

---
+++ RecipeSocialMediaAPI.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+
+namespace RecipeSocialMediaAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
