a6e43373dd970546bb4ed789d1bebdd88e3e8c2b
Andrei Constantin <ac2042@kent.ac.uk>
2023-06-04 14:46:01 +0100

    Added tests for the UserValidationService

---
+++ RecipeSocialMediaAPI.Tests/Unit/UserValidationServiceTests.cs
@@ -0,0 +1,165 @@
+﻿using FluentAssertions;
+using RecipeSocialMediaAPI.Data.DTO;
+using RecipeSocialMediaAPI.Services;
+using BCrypter = BCrypt.Net.BCrypt;
+
+namespace RecipeSocialMediaAPI.Tests.Unit;
+public class UserValidationServiceTests
+{
+    private readonly UserValidationService _userValidationServiceSUT;
+
+    public UserValidationServiceTests()
+    {
+        _userValidationServiceSUT = new UserValidationService();
+    }
+
+    [Fact]
+    public void VerifyPassword_ValidPasswordAndHash_ReturnsTrue()
+    {
+        // Given
+        string password = "P@ssw0rd";
+        string hash = BCrypter.HashPassword(password);
+
+        // When
+        bool result = _userValidationServiceSUT.VerifyPassword(password, hash);
+
+        // Then
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void VerifyPassword_InvalidPasswordAndHash_ReturnsFalse()
+    {
+        // Given
+        string password = "P@ssw0rd";
+        string hash = "wrongly_hashed_password";
+
+        // When
+        bool result = _userValidationServiceSUT.VerifyPassword(password, hash);
+
+        // Then
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("username")]
+    [InlineData("username123")]
+    public void ValidUserName_ValidUserName_ReturnsTrue(string userName)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidUserName(userName);
+
+        // Then
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("invalid_username")]
+    [InlineData("12")]
+    public void ValidUserName_InvalidUserName_ReturnsFalse(string userName)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidUserName(userName);
+
+        // Then
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("test@example.com")]
+    [InlineData("john.doe@example.com")]
+    public void ValidEmail_ValidEmail_ReturnsTrue(string email)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidEmail(email);
+
+        // Then
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("invalid_email")]
+    [InlineData("example.com")]
+    public void ValidEmail_InvalidEmail_ReturnsFalse(string email)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidEmail(email);
+
+        // Then
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("P@ssw0rd")]
+    [InlineData("Password123!")]
+    public void ValidPassword_ValidPassword_ReturnsTrue(string password)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidPassword(password);
+
+        // Then
+        result.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("invalid_password")]
+    [InlineData("12345678")]
+    public void ValidPassword_InvalidPassword_ReturnsFalse(string password)
+    {
+        // Given
+
+        // When
+        bool result = _userValidationServiceSUT.ValidPassword(password);
+
+        // Then
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ValidUser_ValidUserDto_ReturnsTrue()
+    {
+        // Given
+        var userDto = new UserDto
+        {
+            Id = null,
+            UserName = "username",
+            Email = "test@example.com",
+            Password = "P@ssw0rd"
+        };
+
+        // When
+        bool result = _userValidationServiceSUT.ValidUser(userDto);
+
+        // Then
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void ValidUser_InvalidUserDto_ReturnsFalse()
+    {
+        // Given
+        var userDto = new UserDto
+        {
+            Id = null,
+            UserName = "invalid_username",
+            Email = "invalid_email",
+            Password = "invalid_password"
+        };
+
+        // When
+        bool result = _userValidationServiceSUT.ValidUser(userDto);
+
+        // Then
+        result.Should().BeFalse();
+    }
+}
--- RecipeSocialMediaAPI/Services/UserValidationService.cs
+++ RecipeSocialMediaAPI/Services/UserValidationService.cs
@@ -2,33 +2,40 @@
 using RecipeSocialMediaAPI.Data.DTO;
 using BCrypter = BCrypt.Net.BCrypt;
 
-namespace RecipeSocialMediaAPI.Services
+namespace RecipeSocialMediaAPI.Services;
+
+internal class UserValidationService : IUserValidationService
 {
-    internal class UserValidationService : IUserValidationService
+    public string HashPassword(string password) =>
+        BCrypter.HashPassword(password);
+
+    public bool VerifyPassword(string password, string hash)
     {
-        public string HashPassword(string password) =>
-            BCrypter.HashPassword(password);
-
-        public bool VerifyPassword(string password, string hash) =>
-            BCrypter.Verify(password, hash);
-
-        public bool ValidUser(UserDto user) =>
-            ValidUserName(user.UserName)
-            && ValidEmail(user.Email)
-            && ValidPassword(user.Password);
-
-        public bool ValidPassword(string password) => 
-            RegexPatternMatch(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$", password);
-
-        public bool ValidEmail(string email) => 
-            RegexPatternMatch(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)+$", email);
-
-        public bool ValidUserName(string userName) => 
-            RegexPatternMatch(@"^[a-zA-Z0-9]{3,}$", userName);
-
-        private static bool RegexPatternMatch(string pattern, string value) => 
-            new Regex(pattern, RegexOptions.Compiled)
-            .IsMatch(value);
-
+        try
+        {
+            return BCrypter.Verify(password, hash);
+        } 
+        catch (Exception)
+        {
+            return false;
+        }
     }
+
+    public bool ValidUser(UserDto user) =>
+        ValidUserName(user.UserName)
+        && ValidEmail(user.Email)
+        && ValidPassword(user.Password);
+
+    public bool ValidPassword(string password) => 
+        RegexPatternMatch(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$", password);
+
+    public bool ValidEmail(string email) => 
+        RegexPatternMatch(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)+$", email);
+
+    public bool ValidUserName(string userName) => 
+        RegexPatternMatch(@"^[a-zA-Z0-9]{3,}$", userName);
+
+    private static bool RegexPatternMatch(string pattern, string value) => 
+        new Regex(pattern, RegexOptions.Compiled)
+        .IsMatch(value);
 }
