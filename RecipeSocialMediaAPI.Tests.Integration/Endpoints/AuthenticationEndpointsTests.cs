c21ea7734e0ad2edd7bc41a6e8db6d90d1404dbe
Andrei Constantin <ac2042@kent.ac.uk>
2023-06-18 15:52:39 +0100

    Added positive scenario test for Authentication with Email

--- RecipeSocialMediaAPI.Tests.Integration/Endpoints/AuthenticationEndpointsTests.cs
+++ RecipeSocialMediaAPI.Tests.Integration/Endpoints/AuthenticationEndpointsTests.cs
@@ -46,4 +46,41 @@ public class AuthenticationEndpointsTests : EndpointTestBase
         authenticatedUser.Email.Should().Be(userInDb.Email);
         authenticatedUser.Password.Should().Be(userInDb.Password);
     }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, "Authentication")]
+    public async void Authenticate_WhenValidUserWithEmail_ReturnUserFromDB()
+    {
+        // Given
+        UserDto userToCreate = new()
+        {
+            Id = null,
+            UserName = "testUser",
+            Email = "test@mail.com",
+            Password = "Test@123"
+        };
+        var userInDb = (await
+            (await _client
+                .PostAsJsonAsync("user/create", userToCreate))
+            .Content
+            .ReadFromJsonAsync<UserDto>())!;
+
+        // When
+        var result = await _client.PostAsJsonAsync("auth/authenticate", new AuthenticationAttemptDTO()
+        {
+            UsernameOrEmail = userToCreate.Email,
+            Password = userToCreate.Password
+        });
+
+        // Then
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        UserDto authenticatedUser = (await result.Content.ReadFromJsonAsync<UserDto>())!;
+
+        authenticatedUser.Id.Should().Be(userInDb.Id);
+        authenticatedUser.UserName.Should().Be(userInDb.UserName);
+        authenticatedUser.Email.Should().Be(userInDb.Email);
+        authenticatedUser.Password.Should().Be(userInDb.Password);
+    }
+
+
 }
