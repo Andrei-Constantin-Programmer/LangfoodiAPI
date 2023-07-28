facff6b5c93141dc6f5aabd1aa7c2a73abf3c53e
Andrei Constantin <ac2042@kent.ac.uk>
2023-07-28 12:58:15 +0100

    Added initial unit test for the AuthenticateUser handler

---
+++ RecipeSocialMediaAPI.Tests.Unit/Handlers/Authentication/Queries/AuthenticateUserHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Moq;
+using RecipeSocialMediaAPI.DataAccess.Repositories.Interfaces;
+using RecipeSocialMediaAPI.Exceptions;
+using RecipeSocialMediaAPI.Handlers.Authentication.Querries;
+using RecipeSocialMediaAPI.Model;
+
+namespace RecipeSocialMediaAPI.Tests.Unit.Handlers.Authentication.Queries;
+
+public class AuthenticateUserHandlerTests
+{
+    private readonly Mock<IUserRepository> _userRepositoryMock;
+    private readonly Mock<IMapper> _mapper;
+
+    private readonly AuthenticateUserHandler _authenticateUserHandlerSUT;
+
+    public AuthenticateUserHandlerTests()
+    {
+        _userRepositoryMock = new Mock<IUserRepository>();
+        _mapper = new Mock<IMapper>();
+
+        _authenticateUserHandlerSUT = new AuthenticateUserHandler(_userRepositoryMock.Object, _mapper.Object);
+    }
+
+    [Fact]
+    public async Task Handle_WhenUserIsNotFound_ThrowUserNotFoundException()
+    {
+        // Given
+        User? nullUser = null;
+        _userRepositoryMock
+            .Setup(repo => repo.GetUserByUsername(It.IsAny<string>()))
+            .Returns(nullUser);
+        _userRepositoryMock
+            .Setup(repo => repo.GetUserByEmail(It.IsAny<string>()))
+            .Returns(nullUser);
+
+        AuthenticateUserQuery query = new("TestUser", "TestPass");
+
+        // When
+        var action = async () => await _authenticateUserHandlerSUT.Handle(query, CancellationToken.None);
+
+        // Then
+        await action.Should().ThrowAsync<UserNotFoundException>();
+    }
+
+
+}
