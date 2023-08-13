6b64acc225a55c41aa42db79a7ec9c5b8418a886
Unknown807 <milovangveric@gmail.com>
2023-08-13 12:14:02 +0100

    Added EndpointGroups class

---
+++ RecipeSocialMediaAPI.Core/Endpoints/EndpointGroups.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSocialMediaAPI.Core.Contracts;
+using RecipeSocialMediaAPI.Core.DTO;
+using RecipeSocialMediaAPI.Core.Handlers.Users.Commands;
+using RecipeSocialMediaAPI.Core.Handlers.Users.Queries;
+
+namespace RecipeSocialMediaAPI.Core.Endpoints;
+
+public static class EndpointGroups
+{
+    public static RouteGroupBuilder UserEndpoints(this RouteGroupBuilder group)
+    {
+
+        return group;
+    }
+
+    public static RouteGroupBuilder RecipeEndpoints(this RouteGroupBuilder group)
+    {
+
+        return group;
+    }
+
+    public static RouteGroupBuilder AuthenticationEndpoints(this RouteGroupBuilder group)
+    {
+
+        return group;
+    }
+
+    public static RouteGroupBuilder TestEndpoints(this RouteGroupBuilder group)
+    {
+
+        return group;
+    }
+}
