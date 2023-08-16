2832741f0c9e6a9404ab1de7e6530057d9784963
Unknown807 <milovangveric@gmail.com>
2023-08-16 16:58:35 +0100

    Added recipe contracts and amended some DTOs

--- RecipeSocialMediaAPI.Core/Contracts/UpdateRecipeContract.cs
+++ RecipeSocialMediaAPI.Core/Contracts/UpdateRecipeContract.cs
@@ -1,17 +1,14 @@
-﻿using System.Collections.Immutable;
+﻿using RecipeSocialMediaAPI.Core.DTO.Recipes;
+using System.Collections.Immutable;
 
-namespace RecipeSocialMediaAPI.Core.DTO.Recipes;
+namespace RecipeSocialMediaAPI.Core.Contracts;
 
-public record RecipeDetailedDTO
+public record UpdateRecipeContract
 {
     required public int Id { get; set; }
     required public string Title { get; set; }
     required public string Description { get; set; }
-    required public UserDTO Chef { get; set; }
     required public ISet<string> Labels { get; set; }
     required public ImmutableList<IngredientDTO> Ingredients { get; set; }
     required public ImmutableStack<RecipeDTO> RecipeSteps { get; set; }
-    public DateTimeOffset? CreationDate { get; set; }
-    public DateTimeOffset? LastUpdatedDate { get; set; }
 }
-
--- RecipeSocialMediaAPI.Core/DTO/Recipes/RecipeDTO.cs
+++ RecipeSocialMediaAPI.Core/DTO/Recipes/RecipeDTO.cs
@@ -3,12 +3,8 @@
 public record RecipeDTO
 {
     required public int Id { get; set; }
-
     required public string Title { get; set; }
-
     required public string Description { get; set; }
-
     required public string ChefUsername { get; set; }
-
     public DateTimeOffset? CreationDate { get; set; }
 }
