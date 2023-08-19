c38995bf83578bafd26b836220c591daa20b3c0d
Andrei Constantin <ac2042@kent.ac.uk>
2023-08-19 18:00:26 +0100

    Created RecipeRepositoryTests and set up mocks and SUT

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/RecipeRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.DataAccess.Repositories;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Repositories;
+
+public class RecipeRepositoryTests
+{
+    private readonly RecipeRepository _recipeRepositorySUT;
+    private readonly Mock<IMongoCollectionWrapper<RecipeDocument>> _mongoCollectionWrapperMock;
+    private readonly Mock<IMongoCollectionFactory> _mongoCollectionFactoryMock;
+    private readonly Mock<IRecipeDocumentToModelMapper> _mapperMock;
+    
+    public RecipeRepositoryTests() 
+    {
+        _mapperMock = new Mock<IRecipeDocumentToModelMapper>();
+        _mongoCollectionWrapperMock = new Mock<IMongoCollectionWrapper<RecipeDocument>>();
+        _mongoCollectionFactoryMock = new Mock<IMongoCollectionFactory>();
+        _mongoCollectionFactoryMock
+            .Setup(factory => factory.CreateCollection<RecipeDocument>())
+            .Returns(_mongoCollectionWrapperMock.Object);
+
+        _recipeRepositorySUT = new(_mapperMock.Object, _mongoCollectionFactoryMock.Object);
+    }
+
+
+}
