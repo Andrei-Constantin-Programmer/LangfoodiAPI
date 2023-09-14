e89d7a1dd30a3c1fc0715a49478fa84396fe91bd
mg699 <mg699@kent.ac.uk>
2023-09-14 19:08:43 +0100

    Added unit tests for update recipe handler

---
+++ RecipeSocialMediaAPI.Core.Tests.Unit/Handlers/Recipes/Commands/UpdateRecipeHandlerTests.cs
@@ -0,0 +1,157 @@
+﻿using FluentAssertions;
+using Moq;
+using RecipeSocialMediaAPI.Core.Contracts.Recipes;
+using RecipeSocialMediaAPI.Core.DTO.Recipes;
+using RecipeSocialMediaAPI.Core.Exceptions;
+using RecipeSocialMediaAPI.Core.Handlers.Recipes.Commands;
+using RecipeSocialMediaAPI.Core.Mappers.Recipes;
+using RecipeSocialMediaAPI.Core.Utilities;
+using RecipeSocialMediaAPI.DataAccess.Repositories.Interfaces;
+using RecipeSocialMediaAPI.Domain.Mappers.Interfaces;
+using RecipeSocialMediaAPI.Domain.Models.Recipes;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+using RecipeSocialMediaAPI.TestInfrastructure;
+using System.Formats.Tar;
+
+namespace RecipeSocialMediaAPI.Core.Tests.Unit.Handlers.Recipes.Commands;
+public class UpdateRecipeHandlerTests
+{
+    private readonly Mock<IRecipeRepository> _recipeRepositoryMock;
+    private readonly Mock<IRecipeMapper> _recipeMapperMock;
+    private readonly Mock<IIngredientMapper> _ingredientMapperMock;
+    private readonly Mock<IRecipeStepMapper> _recipeStepMapperMock;
+    private readonly Mock<IDateTimeProvider> _timeProviderMock;
+
+    private static readonly DateTimeOffset _testDate = new(2023, 08, 19, 12, 30, 0, TimeSpan.Zero);
+
+    private readonly UpdateRecipeHandler _updateRecipeHandlerSUT;
+
+    public UpdateRecipeHandlerTests()
+    {
+        _recipeMapperMock = new Mock<IRecipeMapper>();
+        _recipeRepositoryMock = new Mock<IRecipeRepository>();
+        _ingredientMapperMock = new Mock<IIngredientMapper>();
+        _recipeStepMapperMock = new Mock<IRecipeStepMapper>();
+        _timeProviderMock = new Mock<IDateTimeProvider>();
+
+        _recipeMapperMock
+            .Setup(x => x.IngredientMapper)
+            .Returns(_ingredientMapperMock.Object);
+
+        _recipeMapperMock
+            .Setup(x => x.RecipeStepMapper)
+            .Returns(_recipeStepMapperMock.Object);
+
+        _timeProviderMock
+            .Setup(x => x.Now)
+            .Returns(_testDate);
+
+        _updateRecipeHandlerSUT = new UpdateRecipeHandler(
+            _recipeMapperMock.Object,
+            _recipeRepositoryMock.Object,
+            _timeProviderMock.Object);
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task Handle_WhenRecipeDoesNotExist_ThrowRecipeNotFoundException()
+    {
+        // Given
+        UpdateRecipeContract testContract = new UpdateRecipeContract()
+        {
+            Id = "1",
+            Title = "Test",
+            Description = "Test",            
+            Labels = new HashSet<string>(),
+            Ingredients = new List<IngredientDTO>(),
+            RecipeSteps = new Stack<RecipeStepDTO>(),
+        };
+
+        // When
+        var action = async () => await _updateRecipeHandlerSUT.Handle(new UpdateRecipeCommand(testContract), CancellationToken.None);
+
+        // Then
+        await action.Should()
+            .ThrowAsync<RecipeNotFoundException>()
+            .WithMessage("The recipe with the id 1 was not found.");
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task Handle_WhenRecipeExistsButUpdateFails_ThrowNewGeneralException()
+    {
+        // Given
+        UpdateRecipeContract testContract = new UpdateRecipeContract()
+        {
+            Id = "1",
+            Title = "Test",
+            Description = "Test",
+            Labels = new HashSet<string>(),
+            Ingredients = new List<IngredientDTO>(),
+            RecipeSteps = new Stack<RecipeStepDTO>(),
+        };
+
+        _recipeRepositoryMock
+            .Setup(x => x.GetRecipeById(It.IsAny<string>()))
+            .Returns(new RecipeAggregate(
+                testContract.Id, testContract.Title,
+                new Recipe(new List<Ingredient>(), new Stack<RecipeStep>()),
+                testContract.Description, new User("1", "user", "mail", "pass"),
+                _testDate, _testDate
+            ));
+
+        _recipeRepositoryMock
+            .Setup(x => x.UpdateRecipe(It.IsAny<RecipeAggregate>()))
+            .Returns(false);
+
+        // When
+        var action = async () => await _updateRecipeHandlerSUT.Handle(new UpdateRecipeCommand(testContract), CancellationToken.None);
+
+        // Then
+        await action.Should()
+            .ThrowAsync<Exception>()
+            .WithMessage("Could not update recipe with id 1.");
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task Handle_WhenRecipeExistsAndUpdateSucceeds_ReturnsCompletedTask()
+    {
+        // Given
+        UpdateRecipeContract testContract = new UpdateRecipeContract()
+        {
+            Id = "1",
+            Title = "Test",
+            Description = "Test",
+            Labels = new HashSet<string>(),
+            Ingredients = new List<IngredientDTO>(),
+            RecipeSteps = new Stack<RecipeStepDTO>(),
+        };
+
+        _recipeRepositoryMock
+            .Setup(x => x.GetRecipeById(It.IsAny<string>()))
+            .Returns(new RecipeAggregate(
+                testContract.Id, testContract.Title,
+                new Recipe(new List<Ingredient>(), new Stack<RecipeStep>()),
+                testContract.Description, new User("1", "user", "mail", "pass"),
+                _testDate, _testDate
+            ));
+
+        _recipeRepositoryMock
+            .Setup(x => x.UpdateRecipe(It.IsAny<RecipeAggregate>()))
+            .Returns(true);
+
+        // When
+        var action = async () => await _updateRecipeHandlerSUT.Handle(new UpdateRecipeCommand(testContract), CancellationToken.None);
+
+        // Then
+        await action.Should()
+            .NotThrowAsync<RecipeNotFoundException>();
+
+        await action.Should()
+            .NotThrowAsync<Exception>();
+    }
+}
