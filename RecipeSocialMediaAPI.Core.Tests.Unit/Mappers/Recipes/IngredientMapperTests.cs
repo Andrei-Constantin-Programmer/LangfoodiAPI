6f78838c345f8ae9a4fd3826e2cf114e733c5291
mg699 <mg699@kent.ac.uk>
2023-09-15 22:28:14 +0100

    Added unit tests for ingredient and recipe step mappers

---
+++ RecipeSocialMediaAPI.Core.Tests.Unit/Mappers/Recipes/IngredientMapperTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using RecipeSocialMediaAPI.Core.DTO.Recipes;
+using RecipeSocialMediaAPI.Core.Mappers.Recipes.Mappers;
+using RecipeSocialMediaAPI.Domain.Models.Recipes;
+using RecipeSocialMediaAPI.TestInfrastructure;
+
+namespace RecipeSocialMediaAPI.Core.Tests.Unit.Mappers.Recipes;
+public class IngredientMapperTests
+{
+    private readonly IngredientMapper _ingredientMapperSUT;
+
+    public IngredientMapperTests()
+    {
+        _ingredientMapperSUT = new IngredientMapper();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void MapIngredientDtoToIngredient_GivenIngredientDto_ReturnIngredient()
+    {
+        // Given
+        IngredientDTO testIngredient = new IngredientDTO()
+        {
+            Name = "eggs",
+            Quantity = 1,
+            UnitOfMeasurement = "whole"
+        };
+
+        Ingredient expectedResult = new Ingredient(
+            testIngredient.Name,
+            testIngredient.Quantity,
+            testIngredient.UnitOfMeasurement
+        );
+
+        // When
+        var result = _ingredientMapperSUT.MapIngredientDtoToIngredient(testIngredient);
+
+        // Then
+        result.Should().Be(expectedResult);
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void MapIngredientToIngredientDto_GivenIngredient_ReturnIngredientDto()
+    {
+        // Given
+        Ingredient testIngredient = new Ingredient("eggs", 1, "whole");
+        
+        IngredientDTO expectedResult = new IngredientDTO()
+        {
+            Name = "eggs",
+            Quantity = 1,
+            UnitOfMeasurement = "whole"
+        };
+
+        // When
+        var result = _ingredientMapperSUT.MapIngredientToIngredientDto(testIngredient);
+
+        // Then
+        result.Should().Be(expectedResult);
+    }
+}
---
+++ RecipeSocialMediaAPI.Core.Tests.Unit/Mappers/Recipes/RecipeStepMapperTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using RecipeSocialMediaAPI.Core.DTO.Recipes;
+using RecipeSocialMediaAPI.Core.Mappers.Recipes.Mappers;
+using RecipeSocialMediaAPI.Domain.Models.Recipes;
+using RecipeSocialMediaAPI.TestInfrastructure;
+
+namespace RecipeSocialMediaAPI.Core.Tests.Unit.Mappers.Recipes;
+public class RecipeStepMapperTests
+{
+    private readonly RecipeStepMapper _recipeStepMapperSUT;
+
+    public RecipeStepMapperTests()
+    {
+        _recipeStepMapperSUT = new RecipeStepMapper();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void MapRecipeStepDtoToRecipeStep_GivenRecipeStepDto_ReturnRecipeStep()
+    {
+        // Given
+        RecipeStepDTO testStep = new RecipeStepDTO()
+        {
+            Text = "Step 1",
+            ImageUrl = "image url"
+        };
+
+        RecipeStep expectedResult = new RecipeStep(testStep.Text, new RecipeImage(testStep.ImageUrl));
+
+        // When
+        var result = _recipeStepMapperSUT.MapRecipeStepDtoToRecipeStep(testStep);
+
+        // Then
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.RECIPE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void MapRecipeStepToRecipeStepDto_GivenRecipeStep_ReturnRecipeStepDto()
+    {
+        // Given
+        RecipeStep testStep = new RecipeStep("Step 1", new RecipeImage("image url"));
+
+        RecipeStepDTO expectedResult = new RecipeStepDTO()
+        {
+            Text = testStep.Text,
+            ImageUrl = testStep.Image.ImageUrl
+        };
+
+        // When
+        var result = _recipeStepMapperSUT.MapRecipeStepToRecipeStepDto(testStep);
+
+        // Then
+        result.Should().BeEquivalentTo(expectedResult);
+    }
+
+}
