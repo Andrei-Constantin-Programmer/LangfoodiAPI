3749eb9e30784c841cba9e39660e937f57fb1c1d
kentfinalproject-admin <ac2042@kent.ac.uk>
2023-10-12 19:08:16 +0100

    Created the MessageDocument

---
+++ RecipeSocialMediaAPI.DataAccess/MongoDocuments/MessageDocument.cs
@@ -0,0 +1,15 @@
+﻿using RecipeSocialMediaAPI.DataAccess.MongoConfiguration;
+
+namespace RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+
+[MongoCollection("Message")]
+public record MessageDocument : MongoDocument
+{
+    required public string SenderId { get; set; }
+    required public MessageContentDTO MessageContent { get; set; }
+    required public DateTimeOffset SentDate { get; set; }
+    public DateTimeOffset? LastUpdatedDate { get; set; } 
+    public string? MessageRepliedToId { get; set; }
+}
+
+public record MessageContentDTO (string? Text = null, List<string>? RecipeIds = null, List<string>? ImageURLs = null);
