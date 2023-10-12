5145352eed3b51582385e7cc9bd329eb77214ad0
kentfinalproject-admin <ac2042@kent.ac.uk>
2023-10-12 19:51:57 +0100

    Implemented the MessageDocumentToModelMapper

---
+++ RecipeSocialMediaAPI.DataAccess/Mappers/Interfaces/IMessageDocumentToModelMapper.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Recipes;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+
+namespace RecipeSocialMediaAPI.DataAccess.Mappers;
+
+public interface IMessageDocumentToModelMapper
+{
+    TextMessage MapMessageDocumentToTextMessage(MessageDocument messageDocument, IUserAccount sender, Message? messageRepliedTo = null);
+    ImageMessage MapMessageDocumentToImageMessage(MessageDocument messageDocument, IUserAccount sender, Message? messageRepliedTo = null);
+    RecipeMessage MapMessageDocumentToRecipeMessage(MessageDocument messageDocument, IUserAccount sender, IEnumerable<RecipeAggregate> recipes, Message? messageRepliedTo = null);
+}
