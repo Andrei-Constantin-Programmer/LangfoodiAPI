da66df2967b52acb4d51b9e819b827bae3b0423e
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-17 11:21:16 +0100

    Created and initialised the MessageQueryRepository

---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/MessageQueryRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecipeSocialMediaAPI.DataAccess.Mappers;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+
+namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+public class MessageQueryRepository
+{
+    private readonly IMessageDocumentToModelMapper _mapper;
+    private readonly IMongoCollectionWrapper<MessageDocument> _messageCollection;
+
+    public MessageQueryRepository(IMessageDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory)
+    {
+        _mapper = mapper;
+        _messageCollection = mongoCollectionFactory.CreateCollection<MessageDocument>();
+    }
+}
