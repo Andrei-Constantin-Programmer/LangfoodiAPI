2e5471545af453fc41368eec7f31732bea3b32f0
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-22 21:03:39 +0100

    Created the UpdateMessage() method

--- RecipeSocialMediaAPI.DataAccess/Repositories/Messages/MessagePersistenceRepository.cs
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/MessagePersistenceRepository.cs
@@ -1,4 +1,5 @@
-﻿using RecipeSocialMediaAPI.DataAccess.Mappers;
+﻿using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.DataAccess.Mappers;
 using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
 using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
@@ -8,11 +9,13 @@ namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
 
 public class MessagePersistenceRepository
 {
+    private readonly ILogger<MessagePersistenceRepository> _logger;
     private readonly IMessageDocumentToModelMapper _mapper;
     private readonly IMongoCollectionWrapper<MessageDocument> _messageCollection;
     
-    public MessagePersistenceRepository(IMessageDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory) 
+    public MessagePersistenceRepository(ILogger<MessagePersistenceRepository> logger, IMessageDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory) 
     {
+        _logger = logger;
         _mapper = mapper;
         _messageCollection = mongoCollectionFactory.CreateCollection<MessageDocument>();
     }
@@ -28,5 +31,34 @@ public class MessagePersistenceRepository
         });
 
         return _mapper.MapMessageFromDocument(messageDocument, sender, messageRepliedTo);
+    }
+
+    public bool UpdateMessage(Message message)
+    {
+        try
+        {
+            return _messageCollection.UpdateRecord(new MessageDocument()
+            {
+                Id = message.Id,
+                SenderId = message.Sender.Id,
+                MessageContent = message switch
+                {
+                    TextMessage textMessage => new(textMessage.TextContent, null, null),
+                    ImageMessage imageMessage => new(imageMessage.TextContent, null, imageMessage.ImageURLs.ToList()),
+                    RecipeMessage recipeMessage => new(recipeMessage.TextContent, recipeMessage.Recipes.Select(recipe => recipe.Id).ToList(), null),
+
+                    _ => throw new Exception($"Unable to update message with id {message.Id}")
+                },
+                SentDate = message.SentDate,
+                LastUpdatedDate = message.UpdatedDate,
+                MessageRepliedToId = message.RepliedToMessage?.Id
+            },
+            doc => doc.Id == message.Id);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "There was a problem updating message {MessageId}: {ErrorMessage}", message.Id, ex.Message);
+            return false;
+        }
     }
 }
