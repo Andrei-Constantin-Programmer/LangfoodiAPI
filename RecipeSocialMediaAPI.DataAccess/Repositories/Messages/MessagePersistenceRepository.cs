6e2a0d22405ff533f19385d98f4fe456defd9a56
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-22 18:05:19 +0100

    Created and initialised the MessagePersistenceRepository

---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/MessagePersistenceRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecipeSocialMediaAPI.DataAccess.Mappers;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+
+namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+public class MessagePersistenceRepository
+{
+    private readonly IMessageDocumentToModelMapper _mapper;
+    private readonly IMongoCollectionWrapper<MessageDocument> _messageCollection;
+
+    public MessagePersistenceRepository(IMessageDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory)
+    {
+        _mapper = mapper;
+        _messageCollection = mongoCollectionFactory.CreateCollection<MessageDocument>();
+    }
+}
