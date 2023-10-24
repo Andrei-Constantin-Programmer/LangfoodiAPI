3af92a6574ea09d7e1b12bb2d0632a8137cd8a5e
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-24 20:16:12 +0100

    Initialised the UpdateMessageHandlerTests class

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/UpdateMessageHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Application.Repositories.Recipes;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Commands;
+
+public class UpdateMessageHandlerTests
+{
+    private readonly Mock<IMessagePersistenceRepository> _messagePersistenceRepositoryMock;
+    private readonly Mock<IMessageQueryRepository> _messageQueryRepositoryMock;
+    private readonly Mock<IRecipeQueryRepository> _recipeQueryRepositoryMock;
+
+    private readonly UpdateMessageHandler _updateMessageHandlerSUT;
+
+    public UpdateMessageHandlerTests()
+    {
+        _messagePersistenceRepositoryMock = new Mock<IMessagePersistenceRepository>();
+        _messageQueryRepositoryMock = new Mock<IMessageQueryRepository>();
+        _recipeQueryRepositoryMock = new Mock<IRecipeQueryRepository>();
+
+        _updateMessageHandlerSUT = new(_messagePersistenceRepositoryMock.Object, _messageQueryRepositoryMock.Object, _recipeQueryRepositoryMock.Object);
+    }
+}
