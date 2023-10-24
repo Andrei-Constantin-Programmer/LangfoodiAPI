546c595415213b40cb06d308abf6de38de164787
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-24 12:02:36 +0100

    Created the GetMessageByIdQuery and MessageDTO

---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetMessageByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+public record GetMessageByIdQuery(string Id) : IRequest<MessageDTO>;
