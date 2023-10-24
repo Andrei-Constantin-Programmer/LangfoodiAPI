a13eb56a9ca14395e3efeabefc41db4328d3cdca
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-24 18:44:13 +0100

    Initialised the UpdateMessageHandler

--- RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/UpdateMessageCommand.cs
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/UpdateMessageCommand.cs
@@ -1,6 +1,28 @@
-﻿using RecipeSocialMediaAPI.Application.Contracts.Messages;
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.Contracts.Messages;
+using RecipeSocialMediaAPI.Application.Mappers.Messages.Interfaces;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
 using RecipeSocialMediaAPI.Application.Validation;
+using RecipeSocialMediaAPI.Domain.Utilities;
 
 namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
 
-public record UpdateMessageCommand(UpdateMessageContract UpdateMessageContract) : IValidatableRequest<UpdateMessageCommand>;
+public record UpdateMessageCommand(UpdateMessageContract UpdateMessageContract) : IValidatableRequest;
+
+internal class UpdateMessageHandler : IRequestHandler<UpdateMessageCommand>
+{
+    private readonly IMessageMapper _mapper;
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IMessagePersistenceRepository _messagePersistenceRepository;
+    private readonly IMessageQueryRepository _messageQueryRepository;
+
+    public UpdateMessageHandler(IMessageMapper mapper, IDateTimeProvider dateTimeProvider, IMessagePersistenceRepository messagePersistenceRepository, IMessageQueryRepository messageQueryRepository)
+    {
+        _mapper = mapper;
+        _dateTimeProvider = dateTimeProvider;
+        _messagePersistenceRepository = messagePersistenceRepository;
+        _messageQueryRepository = messageQueryRepository;
+    }
+
+    public Task Handle(UpdateMessageCommand request, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
