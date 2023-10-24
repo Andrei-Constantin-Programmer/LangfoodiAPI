73cf3a68af584850774542cb16188c1aaf9be2e9
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-24 12:34:18 +0100

    Partially implemented the GetMessageByIdHandler

---
+++ RecipeSocialMediaAPI.Application/Exceptions/MessageNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace RecipeSocialMediaAPI.Application.Exceptions;
+
+public class MessageNotFoundException : Exception
+{
+    public MessageNotFoundException(string id) : base($"The message with the id {id} was not found.") { }
+}
--- RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetMessageByIdQuery.cs
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetMessageByIdQuery.cs
@@ -1,6 +1,28 @@
 ﻿using MediatR;
 using RecipeSocialMediaAPI.Application.DTO.Message;
+using RecipeSocialMediaAPI.Application.Exceptions;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
 
 namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
 
 public record GetMessageByIdQuery(string Id) : IRequest<MessageDTO>;
+
+internal class GetMessageByIdHandler : IRequestHandler<GetMessageByIdQuery, MessageDTO>
+{
+    private readonly IMessageQueryRepository _messageQueryRepository;
+
+    public GetMessageByIdHandler(IMessageQueryRepository messageQueryRepository)
+    {
+        _messageQueryRepository = messageQueryRepository;
+    }
+
+    public Task<MessageDTO> Handle(GetMessageByIdQuery request, CancellationToken cancellationToken)
+    {
+        Message? message = _messageQueryRepository.GetMessage(request.Id);
+
+        return message is null
+            ? throw new MessageNotFoundException(request.Id)
+            : throw new NotImplementedException();
+    }
+}
