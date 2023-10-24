de8756279c43494e0e4ca3ec6be35bb7d3d6d25f
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-24 17:43:53 +0100

    Initialised RemoveMessageHandler and created interface for the MessagePersistenceRepository

--- RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/RemoveMessageCommand.cs
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/RemoveMessageCommand.cs
@@ -1,5 +1,20 @@
 ﻿using MediatR;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
 
 namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
 
 public record RemoveMessageCommand(string Id) : IRequest<bool>;
+
+public class RemoveMessageHandler : IRequestHandler<RemoveMessageCommand, bool>
+{
+    private readonly IMessagePersistenceRepository _messagePersistenceRepository;
+    private readonly IMessageQueryRepository _messageQueryRepository;
+
+    public RemoveMessageHandler(IMessagePersistenceRepository messagePersistenceRepository, IMessageQueryRepository messageQueryRepository)
+    {
+        _messagePersistenceRepository = messagePersistenceRepository;
+        _messageQueryRepository = messageQueryRepository;
+    }
+
+    public Task<bool> Handle(RemoveMessageCommand request, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
---
+++ RecipeSocialMediaAPI.Application/Repositories/Messages/IMessagePersistenceRepository.cs
@@ -0,0 +1,12 @@
+﻿using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+
+namespace RecipeSocialMediaAPI.Application.Repositories.Messages;
+
+public interface IMessagePersistenceRepository
+{
+    Message CreateMessage(IUserAccount sender, string? text, List<string> recipeIds, List<string> imageURLs, DateTimeOffset sentDate, Message? messageRepliedTo);
+    bool UpdateMessage(Message message);
+    bool DeleteMessage(Message message);
+    bool DeleteMessage(string messageId);
+}
