3097e873ce4b7a117b828ee9ab8dc06c4f844be3
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-27 20:42:45 +0100

    Added MapConnectionDocumentFromDocument functionality

--- RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConnectionDocumentToModelMapperTests.cs
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConnectionDocumentToModelMapperTests.cs
@@ -1,6 +1,11 @@
-﻿using Moq;
+﻿using FluentAssertions;
+using Moq;
 using RecipeSocialMediaAPI.Application.Repositories.Users;
 using RecipeSocialMediaAPI.DataAccess.Mappers;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Tests.Shared;
+using RecipeSocialMediaAPI.TestInfrastructure;
 
 namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Mappers;
 
@@ -15,5 +20,65 @@ public class ConnectionDocumentToModelMapperTests
         _userQueryRepositoryMock = new Mock<IUserQueryRepository>();
 
         _connectionDocumentToModelMapperSUT = new(_userQueryRepositoryMock.Object);
+    }
+
+    [Theory]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    [InlineData(ConnectionStatus.Blocked)]
+    [InlineData(ConnectionStatus.Muted)]
+    [InlineData(ConnectionStatus.Pending)]
+    [InlineData(ConnectionStatus.Connected)]
+    [InlineData(ConnectionStatus.Favourite)]
+    public void MapConnectionDocumentFromDocument_WhenDocumentIsValid_ReturnCorrectlyMappedDocument(ConnectionStatus connectionStatus)
+    {
+        // Given
+        TestUserCredentials testUser1 = new()
+        {
+            Account = new TestUserAccount()
+            {
+                Id = "User1",
+                Handler = "user1",
+                UserName = "User 1 Name",
+                AccountCreationDate = new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero)
+            },
+            Email = "user1@mail.com",
+            Password = "password"
+        };
+
+        TestUserCredentials testUser2 = new()
+        {
+            Account = new TestUserAccount()
+            {
+                Id = "User2",
+                Handler = "user2",
+                UserName = "User 2 Name",
+                AccountCreationDate = new(2023, 2, 5, 0, 0, 0, TimeSpan.Zero)
+            },
+            Email = "user2@mail.com",
+            Password = "password"
+        };
+
+        ConnectionDocument testDocument = new()
+        {
+            AccountId1 = testUser1.Account.Id,
+            AccountId2 = testUser2.Account.Id,
+            ConnectionStatus = connectionStatus.ToString()
+        };
+
+        _userQueryRepositoryMock
+            .Setup(repo => repo.GetUserById(testUser1.Account.Id))
+            .Returns(testUser1);
+        _userQueryRepositoryMock
+            .Setup(repo => repo.GetUserById(testUser2.Account.Id))
+            .Returns(testUser2);
+
+        // When
+        var result = _connectionDocumentToModelMapperSUT.MapConnectionDocumentFromDocument(testDocument);
+
+        // Then
+        result.Account1.Should().Be(testUser1.Account);
+        result.Account2.Should().Be(testUser2.Account);
+        result.Status.Should().Be(connectionStatus);
     }
 }
--- RecipeSocialMediaAPI.DataAccess/Mappers/ConnectionDocumentToModelMapper.cs
+++ RecipeSocialMediaAPI.DataAccess/Mappers/ConnectionDocumentToModelMapper.cs
@@ -1,4 +1,6 @@
 ﻿using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
 
 namespace RecipeSocialMediaAPI.DataAccess.Mappers;
 
@@ -9,5 +11,17 @@ internal class ConnectionDocumentToModelMapper
     public ConnectionDocumentToModelMapper(IUserQueryRepository userQueryRepository)
     {
         _userQueryRepository = userQueryRepository;
+    }
+
+    public Connection MapConnectionDocumentFromDocument(ConnectionDocument connectionDocument)
+    {
+        var user1 = _userQueryRepository
+            .GetUserById(connectionDocument.AccountId1)!
+            .Account;
+        var user2 = _userQueryRepository
+            .GetUserById(connectionDocument.AccountId2)!
+            .Account;
+
+        return new Connection(user1, user2, Enum.Parse<ConnectionStatus>(connectionDocument.ConnectionStatus));
     }
 }
