790804cbfc3592f09cc6afcc9761440eb836709e
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-27 20:24:20 +0100

    Created and initialised the ConnectionDocumentToModelMapper class

---
+++ RecipeSocialMediaAPI.DataAccess/Mappers/ConnectionDocumentToModelMapper.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Repositories.Users;
+
+namespace RecipeSocialMediaAPI.DataAccess.Mappers;
+
+internal class ConnectionDocumentToModelMapper
+{
+    private readonly IUserQueryRepository _userQueryRepository;
+
+    public ConnectionDocumentToModelMapper(IUserQueryRepository userQueryRepository)
+    {
+        _userQueryRepository = userQueryRepository;
+    }
+}
