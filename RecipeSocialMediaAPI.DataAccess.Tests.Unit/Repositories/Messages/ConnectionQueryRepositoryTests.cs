acdb5cc93ca70703ff4758efb51f12492fe40338
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-27 21:39:54 +0100

    Created ConnectionQueryRepositoryTests class and initialised the SUT

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/Messages/ConnectionQueryRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Repositories.Messages;
+
+public class ConnectionQueryRepositoryTests
+{
+    private readonly Mock<ILogger<ConnectionQueryRepository>> _loggerMock;
+    private readonly Mock<IConnectionDocumentToModelMapper> _connectionDocumentToModelMapperMock;
+    private readonly Mock<IMongoCollectionWrapper<ConnectionDocument>> _connectionCollectionMock;
+    private readonly Mock<IMongoCollectionFactory> _mongoCollectionFactoryMock;
+
+    private readonly ConnectionQueryRepository _connectionQueryRepositorySUT;
+
+    public ConnectionQueryRepositoryTests()
+    {
+        _loggerMock = new Mock<ILogger<ConnectionQueryRepository>>();
+        _connectionDocumentToModelMapperMock = new Mock<IConnectionDocumentToModelMapper>();
+        _connectionCollectionMock = new Mock<IMongoCollectionWrapper<ConnectionDocument>>();
+        _mongoCollectionFactoryMock = new Mock<IMongoCollectionFactory>();
+        _mongoCollectionFactoryMock
+            .Setup(factory => factory.CreateCollection<ConnectionDocument>())
+            .Returns(_connectionCollectionMock.Object);
+
+        _connectionQueryRepositorySUT = new(_loggerMock.Object, _connectionDocumentToModelMapperMock.Object, _mongoCollectionFactoryMock.Object);
+    }
+}
