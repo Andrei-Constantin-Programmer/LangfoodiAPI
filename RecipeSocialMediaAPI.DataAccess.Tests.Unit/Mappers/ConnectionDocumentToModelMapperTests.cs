742602624e84e0bfb39793249a31c4a03a15d18f
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-27 20:26:20 +0100

    Created and initialised the test class for the ConnectionDocumentToModelMapper

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConnectionDocumentToModelMapperTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.DataAccess.Mappers;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Mappers;
+
+public class ConnectionDocumentToModelMapperTests
+{
+    private readonly Mock<IUserQueryRepository> _userQueryRepositoryMock;
+
+    private readonly ConnectionDocumentToModelMapper _connectionDocumentToModelMapperSUT;
+
+    public ConnectionDocumentToModelMapperTests()
+    {
+        _userQueryRepositoryMock = new Mock<IUserQueryRepository>();
+
+        _connectionDocumentToModelMapperSUT = new(_userQueryRepositoryMock.Object);
+    }
+}
