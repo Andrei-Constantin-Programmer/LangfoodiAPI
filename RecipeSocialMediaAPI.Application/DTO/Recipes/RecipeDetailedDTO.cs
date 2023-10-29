bee226a5b5e5842d40d466d5bd7948850b952841
nlg25 <nlg25@kent.ac.uk>
2023-10-29 12:10:05 +0000

    updated DTO

--- RecipeSocialMediaAPI.Application/DTO/Recipes/RecipeDetailedDTO.cs
+++ RecipeSocialMediaAPI.Application/DTO/Recipes/RecipeDetailedDTO.cs
@@ -15,7 +15,7 @@ public record RecipeDetailedDTO
     required public ISet<string> Labels { get; set; }
     required public List<IngredientDTO> Ingredients { get; set; }
     required public Stack<RecipeStepDTO> RecipeSteps { get; set; }
-    public ServingSize? ServingSize { get; set; }
+    public (double Quantity, string UnitOfMeasurement)? ServingSize { get; set; }
     public DateTimeOffset? CreationDate { get; set; }
     public DateTimeOffset? LastUpdatedDate { get; set; }
 }
