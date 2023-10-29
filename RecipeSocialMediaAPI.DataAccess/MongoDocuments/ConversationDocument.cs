020cade63e7c16ed0f9704f9ce793c59e5b49e84
ff235 <ff235@kent.ac.uk>
2023-10-29 01:24:55 +0000

    Created ConversationDocument. Containing the fields: Connection, GroupId and Messages.

---
+++ RecipeSocialMediaAPI.DataAccess/MongoDocuments/ConversationDocument.cs
@@ -0,0 +1,12 @@
+﻿using RecipeSocialMediaAPI.DataAccess.MongoConfiguration;
+
+namespace RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+
+[MongoCollection("Conversation")]
+public record ConversationDocument : MongoDocument
+{
+    public string? Connection { get; set; }
+    public string? GroupId { get; set; }
+    required public List<string> Messages { get; set; }
+}
+
