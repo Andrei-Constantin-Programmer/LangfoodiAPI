a908c5507fb4d5f6ced5e6e8ccddf52991327040
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-30 19:59:39 +0000

    Created a non-functional implementation of IConnectionPersistenceRepository and added it to the IoC container

---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConnectionPersistenceRepository.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+
+namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+public class ConnectionPersistenceRepository : IConnectionPersistenceRepository
+{
+    public Connection CreateConnection(IUserAccount userAccount1, IUserAccount userAccount2, ConnectionStatus connectionStatus) => throw new NotImplementedException();
+    public bool UpdateConnection(Connection connection) => throw new NotImplementedException();
+    public bool DeleteConnection(Connection connection) => throw new NotImplementedException();
+    public bool DeleteConnection(IUserAccount userAccount1, IUserAccount userAccount2) => throw new NotImplementedException();
+}
