be9330b52071adcce9dfd441321f5cbb33172505
Andrei Constantin <andrei.constantin@outlook.com>
2023-10-30 20:06:37 +0000

    Initialised the ConnectionPersistenceRepository and its respective test class

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/Messages/ConnectionPersistenceRepositoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Repositories.Messages;
+
+public class ConnectionPersistenceRepositoryTests
+{
+    private readonly Mock<ILogger<ConnectionPersistenceRepository>> _loggerMock;
+    private readonly Mock<IConnectionDocumentToModelMapper> _connectionDocumentToModelMapperMock;
+    private readonly Mock<IMongoCollectionWrapper<ConnectionDocument>> _connectionCollectionMock;
+    private readonly Mock<IMongoCollectionFactory> _mongoCollectionFactoryMock;
+
+    private readonly ConnectionPersistenceRepository _connectionPersistenceRepositorySUT;
+
+    public ConnectionPersistenceRepositoryTests()
+    {
+        _loggerMock = new Mock<ILogger<ConnectionPersistenceRepository>>();
+        _connectionDocumentToModelMapperMock = new Mock<IConnectionDocumentToModelMapper>();
+        _connectionCollectionMock = new Mock<IMongoCollectionWrapper<ConnectionDocument>>();
+        _mongoCollectionFactoryMock = new Mock<IMongoCollectionFactory>();
+        _mongoCollectionFactoryMock
+            .Setup(factory => factory.CreateCollection<ConnectionDocument>())
+            .Returns(_connectionCollectionMock.Object);
+
+        _connectionPersistenceRepositorySUT = new ConnectionPersistenceRepository(_loggerMock.Object, _connectionDocumentToModelMapperMock.Object, _mongoCollectionFactoryMock.Object);
+    }
+
+}
--- RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConnectionPersistenceRepository.cs
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConnectionPersistenceRepository.cs
@@ -1,4 +1,8 @@
-﻿using RecipeSocialMediaAPI.Application.Repositories.Messages;
+﻿using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
 using RecipeSocialMediaAPI.Domain.Models.Users;
 
@@ -6,6 +10,17 @@ namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
 
 public class ConnectionPersistenceRepository : IConnectionPersistenceRepository
 {
+    private readonly ILogger<ConnectionPersistenceRepository> _logger;
+    private readonly IConnectionDocumentToModelMapper _mapper;
+    private readonly IMongoCollectionWrapper<ConnectionDocument> _connectionCollection;
+
+    public ConnectionPersistenceRepository(ILogger<ConnectionPersistenceRepository> logger, IConnectionDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory)
+    {
+        _logger = logger;
+        _mapper = mapper;
+        _connectionCollection = mongoCollectionFactory.CreateCollection<ConnectionDocument>();
+    }
+
     public Connection CreateConnection(IUserAccount userAccount1, IUserAccount userAccount2, ConnectionStatus connectionStatus) => throw new NotImplementedException();
     public bool UpdateConnection(Connection connection) => throw new NotImplementedException();
     public bool DeleteConnection(Connection connection) => throw new NotImplementedException();
