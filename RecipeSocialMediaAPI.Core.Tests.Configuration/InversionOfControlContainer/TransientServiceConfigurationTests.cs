18cc8c766c545b25ee6a7715ac5e4436f05c441b
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-01 16:34:26 +0000

    Added TransientServiceConfigurationTests

---
+++ RecipeSocialMediaAPI.Core.Tests.Configuration/InversionOfControlContainer/TransientServiceConfigurationTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using RecipeSocialMediaAPI.TestInfrastructure;
+using RecipeSocialMediaAPI.Application.Cryptography.Interfaces;
+using RecipeSocialMediaAPI.Application.Cryptography;
+using FluentAssertions;
+using RecipeSocialMediaAPI.Domain.Services.Interfaces;
+using RecipeSocialMediaAPI.Domain.Services;
+
+namespace RecipeSocialMediaAPI.Core.Tests.Configuration.InversionOfControlContainer;
+
+public class TransientServiceConfigurationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public TransientServiceConfigurationTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void CryptoService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        using var scope = _factory.Services.CreateScope();
+        var cryptoService = scope.ServiceProvider.GetService(typeof(ICryptoService)) as CryptoService;
+
+        // Then
+        cryptoService.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void MessageFactory_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        using var scope = _factory.Services.CreateScope();
+        var messageFactory = scope.ServiceProvider.GetService(typeof(IMessageFactory)) as MessageFactory;
+
+        // Then
+        messageFactory.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void UserFactory_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        using var scope = _factory.Services.CreateScope();
+        var userFactory = scope.ServiceProvider.GetService(typeof(IUserFactory)) as UserFactory;
+
+        // Then
+        userFactory.Should().NotBeNull();
+    }
+}
