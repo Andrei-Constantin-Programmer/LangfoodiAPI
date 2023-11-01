a57f7702a92156078317c7c4de52fa792aa5c3b8
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-01 16:11:35 +0000

    Added IoC container tests for all singletons

--- RecipeSocialMediaAPI.Core.Tests.Configuration/InversionOfControlContainer/IoCContainerTests.cs
+++ RecipeSocialMediaAPI.Core.Tests.Configuration/InversionOfControlContainer/IoCContainerTests.cs
@@ -1,7 +1,19 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using RecipeSocialMediaAPI.Application.Mappers.Recipes.Interfaces;
+using RecipeSocialMediaAPI.Application.Mappers.Recipes;
+using RecipeSocialMediaAPI.Application.Utilities;
 using RecipeSocialMediaAPI.DataAccess.Helpers;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.Domain.Services;
+using RecipeSocialMediaAPI.Domain.Services.Interfaces;
+using RecipeSocialMediaAPI.Domain.Utilities;
 using RecipeSocialMediaAPI.TestInfrastructure;
+using RecipeSocialMediaAPI.Application.Mappers.Interfaces;
+using RecipeSocialMediaAPI.Application.Mappers.Users;
+using RecipeSocialMediaAPI.Application.Mappers.Messages.Interfaces;
+using RecipeSocialMediaAPI.Application.Mappers.Messages;
 
 namespace RecipeSocialMediaAPI.Core.Tests.Configuration.InversionOfControlContainer;
 
@@ -42,6 +54,89 @@ public class IoCContainerTests : IClassFixture<WebApplicationFactory<Program>>
         cloudinaryConfiguration!.ApiKey.Should().NotBeNullOrWhiteSpace();
         cloudinaryConfiguration!.ApiSecret.Should().NotBeNullOrWhiteSpace();
     }
+    
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void DateTimeProvider_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var dateTimeProvider = _serviceProvider.GetService(typeof(IDateTimeProvider)) as DateTimeProvider;
 
+        // Then
+        dateTimeProvider.Should().NotBeNull();
+        dateTimeProvider!.Now.Should().BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromMinutes(5));
+    }
 
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void UserValidationService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var userValidationService = _serviceProvider.GetService(typeof(IUserValidationService)) as UserValidationService;
+
+        // Then
+        userValidationService.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void RecipeValidationService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var recipeValidationService = _serviceProvider.GetService(typeof(IRecipeValidationService)) as RecipeValidationService;
+
+        // Then
+        recipeValidationService.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void MongoCollectionFactory_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var mongoCollectionFactory = _serviceProvider.GetService(typeof(IMongoCollectionFactory)) as MongoCollectionFactory;
+
+        // Then
+        mongoCollectionFactory.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void RecipeMapperService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var recipeMapper = _serviceProvider.GetService(typeof(IRecipeMapper)) as RecipeMapper;
+
+        // Then
+        recipeMapper.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void UserMapperService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var userMapper = _serviceProvider.GetService(typeof(IUserMapper)) as UserMapper;
+
+        // Then
+        userMapper.Should().NotBeNull();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.CONFIGURATION)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public void MessageMapperService_ShouldBeConfiguredCorrectly()
+    {
+        // Given
+        var messageMapper = _serviceProvider.GetService(typeof(IMessageMapper)) as MessageMapper;
+
+        // Then
+        messageMapper.Should().NotBeNull();
+    }
 }
