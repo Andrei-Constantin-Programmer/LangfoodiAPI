5b487a795f73b1eeada4cf08722215416e074dd8
ff235 <ff235@kent.ac.uk>
2023-11-03 15:04:46 +0000

    Created ConversationDocumentToModelMapper. Contains MapConversationFromDocument, which creates a ConnectionConversation and GroupConversation.

---
+++ RecipeSocialMediaAPI.DataAccess/Mappers/ConversationDocumentToModelMapper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.Application.Repositories.Recipes;
+using RecipeSocialMediaAPI.DataAccess.Exceptions;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Recipes;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+using RecipeSocialMediaAPI.Domain.Services.Interfaces;
+
+namespace RecipeSocialMediaAPI.DataAccess.Mappers;
+
+public class ConversationDocumentToModelMapper : IConversationDocumentToModelMapper
+{
+    public Conversation MapConversationFromDocument(ConversationDocument conversationDocument, Connection? connection, Group? group, List<Message> messages)
+    {
+        {
+            if ((connection is null && group is null) || (connection is not null && group is not null))
+            {
+                throw new MalformedConversationDocumentException(conversationDocument);
+            }
+
+            else if (connection is not null)
+            {
+                new ConnectionConversation(connection, conversationDocument.Id, messages);
+            }
+
+
+            else if (group is not null)
+            {
+                new GroupConversation(group, conversationDocument.Id, messages);
+            }
+
+        }
+
+    }
+}
