3e099a17a6901ac82896dd9030af730af72434b0
ff235 <ff235@kent.ac.uk>
2023-11-03 15:03:55 +0000

    Remove unecessary "using".

--- RecipeSocialMediaAPI.DataAccess/Mappers/Interfaces/IConversationDocumentToModelMapper.cs
+++ RecipeSocialMediaAPI.DataAccess/Mappers/Interfaces/IConversationDocumentToModelMapper.cs
@@ -4,7 +4,6 @@ using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
 // using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
-using RecipeSocialMediaAPI.Domain.Models.Users;
 
 namespace RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
 
