37a5541c06cbda7218daab5995078e7a259960a5
nlg25 <nlg25@kent.ac.uk>
2023-11-05 15:56:36 +0000

    Created GroupDocument in data access

---
+++ RecipeSocialMediaAPI.DataAccess/MongoDocuments/GroupDocument.cs
@@ -0,0 +1,10 @@
+﻿using RecipeSocialMediaAPI.DataAccess.MongoConfiguration;
+
+namespace RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+
+[MongoCollection("Group")]
+public record GroupDocument : MongoDocument
+{
+    required public List<string> AccountIds { get; set; }
+    required public string ConnectionStatus { get; set; }
+}
