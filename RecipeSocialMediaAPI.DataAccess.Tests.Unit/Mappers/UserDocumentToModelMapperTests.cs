c2a7cfb27bd922fcad64550450c7dcf8da0ca0a4
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-06 12:09:30 +0000

    Created and initialised test class for UserDocumentToModelMapper

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/UserDocumentToModelMapperTests.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using RecipeSocialMediaAPI.DataAccess.Mappers;
+using RecipeSocialMediaAPI.Domain.Services.Interfaces;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Mappers;
+
+public class UserDocumentToModelMapperTests
+{
+    private readonly Mock<IUserFactory> _userFactoryMock;
+
+    private readonly UserDocumentToModelMapper _userDocumentToModelMapperSUT;
+
+    public UserDocumentToModelMapperTests()
+    {
+        _userFactoryMock = new Mock<IUserFactory>();
+
+        _userDocumentToModelMapperSUT = new(_userFactoryMock.Object);
+    }
+}
