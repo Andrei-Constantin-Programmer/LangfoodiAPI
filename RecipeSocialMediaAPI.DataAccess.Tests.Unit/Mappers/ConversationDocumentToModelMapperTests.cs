a9cf04a07adc6e4a76ae3a3e0409d0a19cef9d96
ff235 <ff235@kent.ac.uk>
2023-11-06 17:18:19 +0000

    Added `ConversationDocumentToModelMapperTests.cs`

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConversationDocumentToModelMapperTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using RecipeSocialMediaAPI.DataAccess.Exceptions;
+using RecipeSocialMediaAPI.DataAccess.Mappers;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+using RecipeSocialMediaAPI.Domain.Tests.Shared;
+using RecipeSocialMediaAPI.TestInfrastructure;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Mappers;
+
+public class ConversationDocumentToModelMapperTests
+{
+    private readonly ConversationDocumentToModelMapper _conversationDocumentToModelMapperSUT;
+
+    public ConversationDocumentToModelMapperTests()
+    {
+        _conversationDocumentToModelMapperSUT = new ConversationDocumentToModelMapper();
+    }
+
+
+}
