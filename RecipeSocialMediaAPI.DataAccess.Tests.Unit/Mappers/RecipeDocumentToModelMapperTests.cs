a6e2aa0d460d85476ab60e71ce32dc327cf2c0fa
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-06 15:12:18 +0000

    Created and initialised the RecipeDocumentToModelMapper test class

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/RecipeDocumentToModelMapperTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.DataAccess.Mappers;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Mappers;
+
+public class RecipeDocumentToModelMapperTests
+{
+    private readonly RecipeDocumentToModelMapper _recipeDocumentToModelMapperSUT;
+
+    public RecipeDocumentToModelMapperTests()
+    {
+        _recipeDocumentToModelMapperSUT = new RecipeDocumentToModelMapper();
+    }
+}
