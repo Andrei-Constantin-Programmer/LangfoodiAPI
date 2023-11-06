b43fee982a95362ff0a08b6e59e7c25a2c137a66
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-06 11:49:49 +0000

    Added DataAccess and Exception architecture tests

--- RecipeSocialMediaAPI.Core.Tests.Architecture/ArchitectureTests/DataAccessLayerTests.cs
+++ RecipeSocialMediaAPI.Core.Tests.Architecture/ArchitectureTests/DataAccessLayerTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using NetArchTest.Rules;
 using RecipeSocialMediaAPI.Core.Tests.Architecture.TestHelpers;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
 using RecipeSocialMediaAPI.TestInfrastructure;
 
 namespace RecipeSocialMediaAPI.Core.Tests.Architecture.ArchitectureTests;
@@ -21,5 +22,60 @@ public class DataAccessLayerTests
 
         // Then
         testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.ARCHITECTURE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void DataAccessLayer_RepositoriesShouldDependOnBothApplicationAndDomain()
+    {
+        // When
+        var testResult = Types
+            .InAssembly(Assemblies.DataAccessAssembly)
+            .That()
+            .HaveNameEndingWith("Repository")
+            .Should()
+            .HaveDependencyOnAll(Assemblies.APPLICATION, Assemblies.DOMAIN)
+            .GetResult();
+
+        // Then
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.ARCHITECTURE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void DataAccessLayer_MongoDocumentsShouldInheritFromMongoDocumentClass()
+    {
+        // When
+        var testResult = Types
+            .InAssembly(Assemblies.DataAccessAssembly)
+            .That()
+            .HaveNameEndingWith("Document")
+            .And()
+            .DoNotHaveName(nameof(MongoDocument))
+            .Should()
+            .Inherit(typeof(MongoDocument))
+            .GetResult();
+
+        // Then
+        testResult.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.ARCHITECTURE)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void DataAccessLayer_RepositoryInterfacesShouldNotExist()
+    {
+        // When
+        var testResult = Types
+            .InAssembly(Assemblies.DataAccessAssembly)
+            .That()
+            .AreInterfaces()
+            .And()
+            .HaveNameEndingWith("Repository");
+
+        // Then
+        testResult.GetTypes().Should().BeEmpty();
     }
 }
