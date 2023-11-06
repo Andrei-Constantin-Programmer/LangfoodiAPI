686e9ec093af6239361e5b2270e0562dbaaef8ec
ff235 <ff235@kent.ac.uk>
2023-11-06 17:20:07 +0000

    Added test `MapConversationFromDocument_WhenDocumentIsValidAndIsConnectionConversation_ReturnMappedConversation`.

--- RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConversationDocumentToModelMapperTests.cs
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/ConversationDocumentToModelMapperTests.cs
@@ -21,5 +21,52 @@ public class ConversationDocumentToModelMapperTests
         _conversationDocumentToModelMapperSUT = new ConversationDocumentToModelMapper();
     }
 
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    public void MapConversationFromDocument_WhenDocumentIsValidAndIsConnectionConversation_ReturnMappedConversation()
+    {
+        // Given
+        ConversationDocument conversationDocument = new()
+        {
+            Id = "myCoversationID",
+            ConnectionId = "myConnectionID",
+            GroupId = null,
+            Messages = new()
+            {
+                "message1",
+                "message2",
+            }
+        };
+
+        TestUserAccount testUserAccount1 = new()
+        {
+            Id = "user1",
+            Handler = "handler1",
+            UserName = "username1"
+        };
+
+        TestUserAccount testUserAccount2 = new()
+        {
+            Id = "user2",
+            Handler = "handler2",
+            UserName = "username2"
+        };
+
+        Connection connection = new(testUserAccount1, testUserAccount2, ConnectionStatus.Connected);
+
+        List<Message> messages = new()
+        {
+            new TestMessage(conversationDocument.Messages[0],testUserAccount1,new(2023,11,6,0,0,0,TimeSpan.Zero),null),
+            new TestMessage(conversationDocument.Messages[1],testUserAccount2,new(2023,11,6,0,0,0,TimeSpan.Zero),null),
+        };
+
+        // When
+        var result = (ConnectionConversation)_conversationDocumentToModelMapperSUT.MapConversationFromDocument(conversationDocument,connection,null,messages);
+
+        // Then
+        result.ConversationId.Should().Be(conversationDocument.Id);
+        result.Messages.Should().BeEquivalentTo(messages);
+    }
 
 }
