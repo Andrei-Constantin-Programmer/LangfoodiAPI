255d41374d794189d8b08cd1fe123b243fd33f9d
nlg25 <nlg25@kent.ac.uk>
2023-11-12 16:13:22 +0000

    updated users to UserIds

--- RecipeSocialMediaAPI.DataAccess/MongoDocuments/GroupDocument.cs
+++ RecipeSocialMediaAPI.DataAccess/MongoDocuments/GroupDocument.cs
@@ -8,6 +8,6 @@ public record GroupDocument : MongoDocument
     required public string GroupId { get; set; }
     required public string GroupName { get; set; }
     required public string GroupDescription { get; set; }
-    required public List<string> Users { get; set; }    
+    required public List<string> UserIds { get; set; }    
 
 }
