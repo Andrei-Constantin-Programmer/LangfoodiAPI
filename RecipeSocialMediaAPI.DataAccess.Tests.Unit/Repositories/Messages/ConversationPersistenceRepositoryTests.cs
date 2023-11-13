c0d1a6ddae778b63b69a2ec6b6a262611b443882
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-13 17:55:48 +0000

    Injected necessary services (logger, mongo collection, and converation document mapper) in ConversationPersistenceRepository

--- RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/Messages/ConversationPersistenceRepositoryTests.cs
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/Messages/ConversationPersistenceRepositoryTests.cs
@@ -1,13 +1,31 @@
-﻿using RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
 
 namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Repositories.Messages;
 
 public class ConversationPersistenceRepositoryTests
 {
+    private readonly Mock<ILogger<ConversationPersistenceRepository>> _loggerMock;
+    private readonly Mock<IConversationDocumentToModelMapper> _converationDocumentToModelMapperMock;
+    private readonly Mock<IMongoCollectionWrapper<ConversationDocument>> _conversationCollectionMock;
+    private readonly Mock<IMongoCollectionFactory> _mongoCollectionFactoryMock;
+
     private readonly ConversationPersistenceRepository _conversationPersistenceRepositorySUT;
 
     public ConversationPersistenceRepositoryTests()
     {
-        _conversationPersistenceRepositorySUT = new ConversationPersistenceRepository();
+        _loggerMock = new Mock<ILogger<ConversationPersistenceRepository>>();
+        _converationDocumentToModelMapperMock = new Mock<IConversationDocumentToModelMapper>();
+        _conversationCollectionMock = new Mock<IMongoCollectionWrapper<ConversationDocument>>();
+        _mongoCollectionFactoryMock = new Mock<IMongoCollectionFactory>();
+        _mongoCollectionFactoryMock
+            .Setup(factory => factory.CreateCollection<ConversationDocument>())
+            .Returns(_conversationCollectionMock.Object);
+
+        _conversationPersistenceRepositorySUT = new(_loggerMock.Object, _converationDocumentToModelMapperMock.Object, _mongoCollectionFactoryMock.Object);
     }
 }
--- RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationPersistenceRepository.cs
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationPersistenceRepository.cs
@@ -1,4 +1,8 @@
-﻿using RecipeSocialMediaAPI.Application.Repositories.Messages;
+﻿using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
 using RecipeSocialMediaAPI.Domain.Models.Messaging;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
@@ -7,6 +11,17 @@ namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
 
 public class ConversationPersistenceRepository : IConversationPersistenceRepository
 {
+    private readonly ILogger<ConversationPersistenceRepository> _logger;
+    private readonly IConversationDocumentToModelMapper _mapper;
+    private readonly IMongoCollectionWrapper<ConversationDocument> _conversationCollection;
+
+    public ConversationPersistenceRepository(ILogger<ConversationPersistenceRepository> logger, IConversationDocumentToModelMapper mapper, IMongoCollectionFactory mongoCollectionFactory)
+    {
+        _logger = logger;
+        _mapper = mapper;
+        _conversationCollection = mongoCollectionFactory.CreateCollection<ConversationDocument>();
+    }
+
     public Conversation CreateConnectionConversation(Connection connection)
     {
         throw new NotImplementedException();
