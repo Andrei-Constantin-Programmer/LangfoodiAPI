63fd81fbd7313160a4c40c15f44fc6e379490405
ff235 <ff235@kent.ac.uk>
2023-11-13 11:19:44 +0000

    Added GetDetailedMessageQuery.cs

---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetDetailedMessageQuery.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+public record GetDetailedMessageQuery(string Id) : IRequest<MessageDTO?>;
+
+internal class GetDetailedMessageHandler
+{
+    public GetDetailedMessageHandler()
+    {
+
+    }
+
+    public async Task<> Handle()
+    {
+        return null;
+    }
+
+}
