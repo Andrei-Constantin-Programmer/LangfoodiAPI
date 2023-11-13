09a5b83448e5b074653e1621056ae8f26ff54a3c
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-13 17:50:09 +0000

    Created and initialised ConversationPersistenceRepository and its test class

---
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Repositories/Messages/ConversationPersistenceRepositoryTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+namespace RecipeSocialMediaAPI.DataAccess.Tests.Unit.Repositories.Messages;
+
+public class ConversationPersistenceRepositoryTests
+{
+    private readonly ConversationPersistenceRepository _conversationPersistenceRepositorySUT;
+
+    public ConversationPersistenceRepositoryTests()
+    {
+        _conversationPersistenceRepositorySUT = new ConversationPersistenceRepository();
+    }
+}
---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationPersistenceRepository.cs
@@ -0,0 +1,34 @@
+﻿using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Messaging;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
+
+namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+public class ConversationPersistenceRepository : IConversationPersistenceRepository
+{
+    public Conversation CreateConnectionConversation(Connection connection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Conversation CreateGroupConversation(Group group)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Conversation UpdateConversation(Conversation conversation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteConversation(Conversation conversation)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteConversation(string conversationId)
+    {
+        throw new NotImplementedException();
+    }
+}
