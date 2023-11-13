00d2663dbd3599d33c039fa6427417cbc1e4e0bb
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-13 17:46:04 +0000

    Added the IConversationPersistenceRepository interface

---
+++ RecipeSocialMediaAPI.Application/Repositories/Messages/IConversationPersistenceRepository.cs
@@ -0,0 +1,14 @@
+﻿using RecipeSocialMediaAPI.Domain.Models.Messaging;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
+
+namespace RecipeSocialMediaAPI.Application.Repositories.Messages;
+
+public interface IConversationPersistenceRepository
+{
+    public Conversation CreateConnectionConversation(Connection connection);
+    public Conversation CreateGroupConversation(Group group);
+    public Conversation UpdateConversation(Conversation conversation);
+    public bool DeleteConversation(Conversation conversation);
+    public bool DeleteConversation(string conversationId);
+}
