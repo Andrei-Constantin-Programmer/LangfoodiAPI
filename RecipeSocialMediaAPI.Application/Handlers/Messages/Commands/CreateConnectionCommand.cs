86af23d144961931b7f166bcbcff42f740085bf7
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-17 15:28:50 +0000

    Created ConnectionDTO and CreateConnectionCommand

---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/CreateConnectionCommand.cs
@@ -0,0 +1,6 @@
+﻿using RecipeSocialMediaAPI.Application.DTO.Message;
+using RecipeSocialMediaAPI.Application.Validation;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+public record CreateConnectionCommand() : IValidatableRequest<ConnectionDTO>;
