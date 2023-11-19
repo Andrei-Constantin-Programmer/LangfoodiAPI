0e0038165a5bf6c87328212be065d367fb94024b
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-19 16:36:53 +0000

    Set up the UpdateConnectionCommand and its handler

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/UpdateConnectionHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Commands;
+
+public class UpdateConnectionHandlerTests
+{
+    private readonly UpdateConnectionHandler _updateConnectionHandlerSUT;
+
+    public UpdateConnectionHandlerTests()
+    {
+        _updateConnectionHandlerSUT = new();
+    }
+}
---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/UpdateConnectionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.Contracts.Messages;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+public record UpdateConnectionCommand(UpdateConnectionContract UpdateConnectionContract) : IRequest<bool>;
+
+internal class UpdateConnectionHandler : IRequestHandler<UpdateConnectionCommand, bool>
+{
+    public Task<bool> Handle(UpdateConnectionCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
