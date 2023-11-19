540d596d733a6fe68af721aa61097765acd9745a
Andrei Constantin <andrei.constantin@outlook.com>
2023-11-19 17:24:46 +0000

    Implemented the happy path for the UpdateConnectionHandler

--- RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/UpdateConnectionCommand.cs
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/UpdateConnectionCommand.cs
@@ -1,5 +1,9 @@
 ﻿using MediatR;
 using RecipeSocialMediaAPI.Application.Contracts.Messages;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Users;
 
 namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
 
@@ -7,8 +11,27 @@ public record UpdateConnectionCommand(UpdateConnectionContract UpdateConnectionC
 
 internal class UpdateConnectionHandler : IRequestHandler<UpdateConnectionCommand, bool>
 {
+    private readonly IConnectionQueryRepository _connectionQueryRepository;
+    private readonly IConnectionPersistenceRepository _connectionPersistenceRepository;
+    private readonly IUserQueryRepository _userQueryRepository;
+
+    public UpdateConnectionHandler(IConnectionQueryRepository connectionQueryRepository, IConnectionPersistenceRepository connectionPersistenceRepository, IUserQueryRepository userQueryRepository)
+    {
+        _connectionQueryRepository = connectionQueryRepository;
+        _connectionPersistenceRepository = connectionPersistenceRepository;
+        _userQueryRepository = userQueryRepository;
+    }
+
     public Task<bool> Handle(UpdateConnectionCommand request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        IUserAccount user1 = _userQueryRepository.GetUserById(request.UpdateConnectionContract.UserId1)!.Account;
+        IUserAccount user2 = _userQueryRepository.GetUserById(request.UpdateConnectionContract.UserId2)!.Account;
+
+        IConnection connection = _connectionQueryRepository.GetConnection(user1, user2)!;
+
+        Enum.TryParse(request.UpdateConnectionContract.NewConnectionStatus, out ConnectionStatus status);
+        connection.Status = status;
+            
+        return Task.FromResult(_connectionPersistenceRepository.UpdateConnection(connection));
     }
 }
