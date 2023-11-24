c745cf9f1b5f932b7be9a1e4bad4db5cbaf1674d
mg699 <mg699@kent.ac.uk>
2023-11-24 18:51:41 +0000

    Added options validator for cloudinary endpoint options and have implemented the cloudinary web client file for communicating with Cloudinary's API

--- RecipeSocialMediaAPI.Application/WebClients/CloudinaryWebClient.cs
+++ RecipeSocialMediaAPI.Application/WebClients/CloudinaryWebClient.cs
@@ -1,24 +1,25 @@
-﻿using RecipeSocialMediaAPI.Application.DTO.ImageHosting;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
+using RecipeSocialMediaAPI.Application.DTO.ImageHosting;
+using RecipeSocialMediaAPI.Application.Options;
 using System.Net.Http.Headers;
 using System.Text;
-using RecipeSocialMediaAPI.Application.Options;
 
 namespace RecipeSocialMediaAPI.Application.WebClients;
 public class CloudinaryWebClient
 {
+    private readonly CloudinaryEndpointOptions _cloudinaryEndpoints;
  
     public CloudinaryWebClient(IOptions<CloudinaryEndpointOptions> cloudinaryOptions)
     {
-
+        _cloudinaryEndpoints = cloudinaryOptions.Value;
     }
 
-    public bool RemoveHostedImage(CloudinarySignatureDTO signature, string publicId)
+    public bool RemoveHostedImage(CloudinarySignatureDTO signature, string apiKey, string publicId)
     {
         using var httpClient = new HttpClient();
         using var request = new HttpRequestMessage(
             new HttpMethod("POST"),
-            $"https://api.cloudinary.com/v1_1/{_cloudinaryConfig.CloudName}/image/destroy" +
+            _cloudinaryEndpoints.SingleRemoveUrl +
             $"?public_id={publicId}&api_key={apiKey}&signature={signature.Signature}&timestamp={signature.TimeStamp}"
         );
 
@@ -26,17 +27,15 @@ public class CloudinaryWebClient
         return response.IsSuccessStatusCode;
     }
 
-    public bool BulkRemoveHostedImages(List<string> publicIds)
+    public bool BulkRemoveHostedImages(List<string> publicIds, string apiKey, string apiSecret)
     {
-        // TODO: change to use service/smth instead
-
         using var httpClient = new HttpClient();
         using var request = new HttpRequestMessage(
-            new System.Net.Http.HttpMethod("DELETE"),
-            $"https://api.cloudinary.com/v1_1/{_cloudinaryConfig.CloudName}/resources/image/upload"
+            new HttpMethod("DELETE"),
+            _cloudinaryEndpoints.BulkRemoveUrl
         );
 
-        var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{_cloudinaryConfig.ApiKey}:{_cloudinaryConfig.ApiSecret}"));
+        var base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{apiKey}:{apiSecret}"));
         request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
         request.Content = new StringContent($"public_ids[]={string.Join("&public_ids[]=", publicIds)}");
         request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
---
+++ RecipeSocialMediaAPI.Core/OptionValidation/CloudinaryEndpointOptionValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using RecipeSocialMediaAPI.Application.Options;
+
+namespace RecipeSocialMediaAPI.Core.OptionValidation;
+
+public sealed class CloudinaryEndpointOptionValidator : AbstractValidator<CloudinaryEndpointOptions>
+{
+    public CloudinaryEndpointOptionValidator()
+    {
+        RuleFor(x => x.SingleRemoveUrl)
+            .NotEmpty()
+            .WithMessage("The URL for single image removal cannot be empty.")
+            .Must(url => Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            .WithMessage("The URL for single image removal must be correctly formatted.");
+
+        RuleFor(x => x.BulkRemoveUrl)
+            .NotEmpty()
+            .WithMessage("The URL for bulk image removal cannot be empty.")
+            .Must(url => Uri.TryCreate(url, UriKind.Absolute, out var uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            .WithMessage("The URL for bulk image removal must be correctly formatted.");
+    }
+}
