d5d32432126f6be082fb55e04c835a9b307261e8
ff235 <ff235@kent.ac.uk>
2023-11-26 15:47:53 +0000

    Added GetMessageDetailedHandlerTests class and defined constructor

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Queries/GetMessageDetailedHandlerTests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Moq;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+using RecipeSocialMediaAPI.Application.Exceptions;
+using RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+using RecipeSocialMediaAPI.Application.Mappers.Messages.Interfaces;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Messages;
+using RecipeSocialMediaAPI.Domain.Tests.Shared;
+using RecipeSocialMediaAPI.TestInfrastructure;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Queries;
+
+public class GetMessageDetailedHandlerTests
+{
+    private readonly Mock<IMessageMapper> _messageMapperMock;
+    private readonly Mock<IMessageQueryRepository> _messageQueryRepository;
+    private readonly GetMessageDetailedByIdHandler _getMessageDetailedByIdHandlerSUT;
+
+    public GetMessageDetailedHandlerTests()
+    {
+        _messageMapperMock = new Mock<IMessageMapper>();
+        _messageQueryRepository = new Mock<IMessageQueryRepository>();
+        _getMessageDetailedByIdHandlerSUT = new(_messageMapperMock.Object, _messageQueryRepository.Object);
+    }
+
+
+}
