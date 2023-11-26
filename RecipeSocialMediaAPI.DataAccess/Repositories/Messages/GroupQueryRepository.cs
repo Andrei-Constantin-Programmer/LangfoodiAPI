751dff4bd584499045583988f0245a7694dfaf27
nlg25 <nlg25@kent.ac.uk>
2023-11-26 18:32:54 +0000

    created groupQueryRepository class and constructor for database access

---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/GroupQueryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+using System.Text.RegularExpressions;
+
+namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+internal class GroupQueryRepository
+{
+    private readonly ILogger<GroupQueryRepository> _logger;
+    private readonly IGroupDocumentToModelMapper _mapper;
+    private readonly IMongoCollectionWrapper<GroupDocument> _groupCollection;
+
+    public GroupQueryRepository(ILogger<GroupQueryRepository> logger, IGroupDocumentToModelMapper groupDocumentToModelMapper, IMongoCollectionFactory mongoCollectionFactory)
+    {
+        _logger = logger;
+        _mapper = groupDocumentToModelMapper;
+        _groupCollection = mongoCollectionFactory.CreateCollection<GroupDocument>();
+    }
+}
