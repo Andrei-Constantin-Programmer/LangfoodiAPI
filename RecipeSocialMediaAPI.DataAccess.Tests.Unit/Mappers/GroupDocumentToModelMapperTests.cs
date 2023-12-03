6fa02a4d2a0ad7209fa8b9ec05e2c61b299b0a75
nlg25 <nlg25@kent.ac.uk>
2023-12-03 19:40:44 +0000

    Added test for GroupDocumentToModelMapper for when Users dont exist

--- RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/GroupDocumentToModelMapperTests.cs
+++ RecipeSocialMediaAPI.DataAccess.Tests.Unit/Mappers/GroupDocumentToModelMapperTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using Moq;
 using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.DataAccess.Exceptions;
 using RecipeSocialMediaAPI.DataAccess.Mappers;
 using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
 using RecipeSocialMediaAPI.Domain.Models.Users;
@@ -78,5 +79,70 @@ public class GroupDocumentToModelMapperTests
         result.GroupName.Should().Be(testDocument.GroupName);
         result.GroupDescription.Should().Be(testDocument.GroupDescription);
         result.Users.Should().BeEquivalentTo(new List<IUserAccount>() { testUser1.Account, testUser2.Account });
+    }
+
+    [Theory]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.DATA_ACCESS)]
+    [InlineData(false, true)]
+    [InlineData(true, false)]
+    [InlineData(false, false)]
+    public void MapGroupFromDocument_WhenUsersDontExist_UserDocumentNotFoundException(bool firstUserExists, bool secondUserExists)
+    {
+        // Given
+        TestUserCredentials? testUser1 =
+            firstUserExists ? new()
+            {
+                Account = new TestUserAccount()
+                {
+                    Id = "User1",
+                    Handler = "user1",
+                    UserName = "User 1 Name",
+                    AccountCreationDate = new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero)
+                },
+                Email = "user1@mail.com",
+                Password = "password"
+            }
+            : null;
+
+        TestUserCredentials? testUser2 =
+            secondUserExists ? new()
+            {
+                Account = new TestUserAccount()
+                {
+                    Id = "User2",
+                    Handler = "user2",
+                    UserName = "User 2 Name",
+                    AccountCreationDate = new(2023, 2, 5, 0, 0, 0, TimeSpan.Zero)
+                },
+                Email = "user2@mail.com",
+                Password = "password"
+            }
+            : null;
+
+        string userId1 = testUser1?.Account.Id ?? "User1Id";
+        string userId2 = testUser2?.Account.Id ?? "User2Id";
+
+        GroupDocument testDocument = new()
+        {
+            Id = "1",
+            GroupId = "1",
+            GroupName = "Test Name",
+            GroupDescription = "Test Desc",
+            UserIds = new() { userId1, userId2 }
+        };
+        _userQueryRepositoryMock
+            .Setup(repo => repo.GetUserById(userId1))
+            .Returns(testUser1);
+        _userQueryRepositoryMock
+            .Setup(repo => repo.GetUserById(userId2))
+            .Returns(testUser2);
+
+        // When
+        var result = () => _groupDocumentToModelMapperSUT.MapGroupFromDocument(testDocument);
+
+        // Then
+        result.Should().Throw<UserDocumentNotFoundException>();
+
     }
 }
