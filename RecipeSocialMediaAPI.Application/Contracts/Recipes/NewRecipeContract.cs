68fe4b9a7c14845dc7cb6d05ea0b6b36165ad633
mg699 <mg699@kent.ac.uk>
2023-12-06 18:04:49 +0000

    Split serving size into two different fields for relevant recipe contracts and dtos

--- RecipeSocialMediaAPI.Application/Contracts/Recipes/NewRecipeContract.cs
+++ RecipeSocialMediaAPI.Application/Contracts/Recipes/NewRecipeContract.cs
@@ -15,5 +15,7 @@ public record NewRecipeContract
     public int? KiloCalories { get; set; }
     required public List<IngredientDTO> Ingredients { get; set; }
     required public Stack<RecipeStepDTO> RecipeSteps { get; set; }
-    public (double Quantity, string UnitOfMeasurement)? ServingSize { get; set; }
+    public double? ServingQuantity { get; set; }
+    public string? ServingUnitOfMeasurement { get; set; }
 }
+
--- RecipeSocialMediaAPI.Application/DTO/Recipes/RecipeDetailedDTO.cs
+++ RecipeSocialMediaAPI.Application/DTO/Recipes/RecipeDetailedDTO.cs
@@ -1,5 +1,4 @@
 ﻿using RecipeSocialMediaAPI.Application.DTO.Users;
-using RecipeSocialMediaAPI.Domain.Models.Recipes;
 
 namespace RecipeSocialMediaAPI.Application.DTO.Recipes;
 
@@ -16,7 +15,8 @@ public record RecipeDetailedDTO
     required public ISet<string> Labels { get; set; }
     required public List<IngredientDTO> Ingredients { get; set; }
     required public Stack<RecipeStepDTO> RecipeSteps { get; set; }
-    public (double Quantity, string UnitOfMeasurement)? ServingSize { get; set; }
+    public double? ServingQuantity { get; set; }
+    public string? ServingUnitOfMeasurement { get; set; }
     public DateTimeOffset? CreationDate { get; set; }
     public DateTimeOffset? LastUpdatedDate { get; set; }
 }
