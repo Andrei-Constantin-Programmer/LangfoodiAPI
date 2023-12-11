f09103267fc1bff140c531c88ee07f26a189c05e
ff235 <ff235@kent.ac.uk>
2023-12-11 01:47:40 +0000

    Added `ConversationQueryRepository.cs`

---
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationQueryRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
+
+public class ConversationQueryRepository : IMessageQueryRepository
+{
+
+    public ConversationQueryRepository()
+    {
+
+    }
+
+    public Conversation? GetConversationById(string id)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+
+    public List<Conversation> GetConversationsByUser(IUserAccount userAccount)
+    {
+        // TODO: Implement
+        throw new NotImplementedException();
+    }
+}
