99b8d30569f2a98534e423db1f8c4a50a74d8912
ff235 <ff235@kent.ac.uk>
2023-12-11 02:13:39 +0000

    Add use of namespaces

--- RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationQueryRepository.cs
+++ RecipeSocialMediaAPI.DataAccess/Repositories/Messages/ConversationQueryRepository.cs
@@ -1,4 +1,13 @@
 ﻿using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration;
+using RecipeSocialMediaAPI.DataAccess.MongoConfiguration.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.DataAccess.Mappers;
+using Microsoft.Extensions.Logging;
+using RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+using RecipeSocialMediaAPI.Domain.Models.Users;
 
 namespace RecipeSocialMediaAPI.DataAccess.Repositories.Messages;
 
