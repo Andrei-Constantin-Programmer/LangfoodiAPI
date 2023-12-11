4882fbe0717fc3ff6956e62c480d1e5933c25dfb
ff235 <ff235@kent.ac.uk>
2023-12-11 02:12:29 +0000

    Added the interface `IConversationDocumentToModelMapper.cs`

---
+++ RecipeSocialMediaAPI.DataAccess/Mappers/Interfaces/IConversationDocumentToModelMapper.cs
@@ -0,0 +1,8 @@
+﻿using RecipeSocialMediaAPI.DataAccess.MongoDocuments;
+using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
+
+namespace RecipeSocialMediaAPI.DataAccess.Mappers.Interfaces;
+public interface IConversationDocumentToModelMapper
+{
+    ConversationDocument MapConversationFromDocument(ConversationDocument conversationDocument);
+}
