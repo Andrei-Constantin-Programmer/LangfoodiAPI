40f6a69df5d7fcaaae33c42028cd6a6f90fce4a7
mg699 <mg699@kent.ac.uk>
2023-12-21 14:22:22 +0000

    Copied SecureEquals method from Bcrypt library in order to verify matching hashes in CryptoService

--- RecipeSocialMediaAPI.Application/Cryptography/CryptoService.cs
+++ RecipeSocialMediaAPI.Application/Cryptography/CryptoService.cs
@@ -1,15 +1,22 @@
 ﻿using RecipeSocialMediaAPI.Application.Cryptography.Interfaces;
+using System.Runtime.CompilerServices;
+using System.Text;
 using BCrypter = BCrypt.Net.BCrypt;
 
 namespace RecipeSocialMediaAPI.Application.Cryptography;
 
 public class CryptoService : ICryptoService
 {
-    public bool ArePasswordsTheSame(string decryptedPassword, string encryptedPassword)
+    private readonly Encoding _safeUTF8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+
+    public bool ArePasswordsTheSame(string clientPassword, string serverPassword)
     {
         try
         {
-            return BCrypter.Verify(decryptedPassword, encryptedPassword);
+            return BCrypter.Verify(clientPassword, serverPassword)
+                || SecureEquals(
+                    _safeUTF8.GetBytes(clientPassword),
+                    _safeUTF8.GetBytes(serverPassword));
         }
         catch (Exception)
         {
@@ -20,5 +27,28 @@ public class CryptoService : ICryptoService
     public string Encrypt(string password)
     {
         return BCrypter.HashPassword(password);
+    }
+
+    // This method is taken directly from the BCrypt library, with no alterations
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    private bool SecureEquals(byte[] a, byte[] b)
+    {
+        if (a == null && b == null)
+        {
+            return true;
+        }
+
+        if (a == null || b == null || a.Length != b.Length)
+        {
+            return false;
+        }
+
+        int num = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            num |= a[i] ^ b[i];
+        }
+
+        return num == 0;
     }
 }
