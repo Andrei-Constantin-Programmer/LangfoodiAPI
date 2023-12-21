4dd967770cbf11fc6655d52e4eede3859c2803fa
mg699 <mg699@kent.ac.uk>
2023-12-21 14:38:33 +0000

    Removed secure equals, standard string comparison will do

--- RecipeSocialMediaAPI.Application/Cryptography/CryptoService.cs
+++ RecipeSocialMediaAPI.Application/Cryptography/CryptoService.cs
@@ -7,16 +7,13 @@ namespace RecipeSocialMediaAPI.Application.Cryptography;
 
 public class CryptoService : ICryptoService
 {
-    private readonly Encoding _safeUTF8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
 
     public bool ArePasswordsTheSame(string clientPassword, string serverPassword)
     {
         try
         {
             return BCrypter.Verify(clientPassword, serverPassword)
-                || SecureEquals(
-                    _safeUTF8.GetBytes(clientPassword),
-                    _safeUTF8.GetBytes(serverPassword));
+                   || clientPassword == serverPassword;
         }
         catch (Exception)
         {
@@ -27,28 +24,5 @@ public class CryptoService : ICryptoService
     public string Encrypt(string password)
     {
         return BCrypter.HashPassword(password);
-    }
-
-    // This method is taken directly from the BCrypt library, with no alterations
-    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
-    private bool SecureEquals(byte[] a, byte[] b)
-    {
-        if (a == null && b == null)
-        {
-            return true;
-        }
-
-        if (a == null || b == null || a.Length != b.Length)
-        {
-            return false;
-        }
-
-        int num = 0;
-        for (int i = 0; i < a.Length; i++)
-        {
-            num |= a[i] ^ b[i];
-        }
-
-        return num == 0;
     }
 }
