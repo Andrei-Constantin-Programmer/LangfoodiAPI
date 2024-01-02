a29923fc068089e00d154527bfaacdd7953272bf
Andrei Constantin <andrei.constantin@outlook.com>
2024-01-02 15:53:14 +0000

    Created the GetConnectionsByUser query and handler

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Queries/GetConnectionsByUserHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Queries;
+
+public class GetConnectionsByUserHandlerTests
+{
+    private readonly GetConnectionsByUserHandler _getConnectionsByUserHandlerSUT;
+
+    public GetConnectionsByUserHandlerTests()
+    {
+        _getConnectionsByUserHandlerSUT = new();
+    }
+}
---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetConnectionsByUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+public record GetConnectionsByUserQuery(string UserId) : IRequest<IEnumerable<ConnectionDTO>>;
+
+internal class GetConnectionsByUserHandler : IRequestHandler<GetConnectionsByUserQuery, IEnumerable<ConnectionDTO>>
+{
+    public Task<IEnumerable<ConnectionDTO>> Handle(GetConnectionsByUserQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
