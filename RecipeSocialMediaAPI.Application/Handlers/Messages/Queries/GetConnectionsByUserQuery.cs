0d5f861ef5af8bc8c444c744fef2fcff2602fc48
Andrei Constantin <andrei.constantin@outlook.com>
2024-01-02 16:36:46 +0000

    Implemented the GetConnectionsByUser handler

--- RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetConnectionsByUserQuery.cs
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetConnectionsByUserQuery.cs
@@ -1,5 +1,9 @@
 ﻿using MediatR;
 using RecipeSocialMediaAPI.Application.DTO.Message;
+using RecipeSocialMediaAPI.Application.Exceptions;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.Domain.Models.Users;
 
 namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
 
@@ -7,8 +11,22 @@ public record GetConnectionsByUserQuery(string UserId) : IRequest<IEnumerable<Co
 
 internal class GetConnectionsByUserHandler : IRequestHandler<GetConnectionsByUserQuery, IEnumerable<ConnectionDTO>>
 {
+    private readonly IUserQueryRepository _userQueryRepository;
+    private readonly IConnectionQueryRepository _connectionQueryRepository;
+
+    public GetConnectionsByUserHandler(IUserQueryRepository userQueryRepository, IConnectionQueryRepository connectionQueryRepository)
+    {
+        _userQueryRepository = userQueryRepository;
+        _connectionQueryRepository = connectionQueryRepository;
+    }
+
     public Task<IEnumerable<ConnectionDTO>> Handle(GetConnectionsByUserQuery request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        IUserAccount user = _userQueryRepository.GetUserById(request.UserId)?.Account
+            ?? throw new UserNotFoundException($"No user found with id {request.UserId}");
+
+        return Task.FromResult(_connectionQueryRepository
+            .GetConnectionsForUser(user)
+            .Select(connection => new ConnectionDTO(connection.Account1.Id, connection.Account2.Id, connection.Status.ToString())));
     }
 }
