ccebcab5d77af1c64185f66b249b7e8160a20267
Andrei Constantin <andrei.constantin@outlook.com>
2024-01-10 16:58:19 +0000

    Created and initialised the DeleteGroupCommand and its respective test class

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/DeleteGroupHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Commands;
+
+public class DeleteGroupHandlerTests
+{
+    private readonly DeleteGroupHandler _deleteGroupHandlerSUT;
+
+    public DeleteGroupHandlerTests()
+    {
+        _deleteGroupHandlerSUT = new();
+    }
+}
---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/DeleteGroupCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+public record DeleteGroupCommand(string GroupId) : IRequest<bool>;
+
+internal class DeleteGroupHandler : IRequestHandler<DeleteGroupCommand, bool>
+{
+    public Task<bool> Handle(DeleteGroupCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
