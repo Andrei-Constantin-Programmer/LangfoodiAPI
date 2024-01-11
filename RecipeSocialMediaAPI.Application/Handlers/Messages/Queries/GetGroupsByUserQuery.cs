c8d6f00cea770a136b8c2def57e7e66c0857de8d
Andrei Constantin <andrei.constantin@outlook.com>
2024-01-11 18:38:38 +0000

    Implemented GetGroupsByUserHandler

---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetGroupsByUserQuery.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+using RecipeSocialMediaAPI.Application.Exceptions;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+using RecipeSocialMediaAPI.Application.Repositories.Users;
+using RecipeSocialMediaAPI.Domain.Models.Users;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+public record GetGroupsByUserQuery(string UserId) : IRequest<IEnumerable<GroupDTO>>;
+
+internal class GetGroupsByUserHandler : IRequestHandler<GetGroupsByUserQuery, IEnumerable<GroupDTO>>
+{
+    private readonly IGroupQueryRepository _groupQueryRepository;
+    private readonly IUserQueryRepository _userQueryRepository;
+
+    public GetGroupsByUserHandler(IGroupQueryRepository groupQueryRepository, IUserQueryRepository userQueryRepository)
+    {
+        _groupQueryRepository = groupQueryRepository;
+        _userQueryRepository = userQueryRepository;
+    }
+
+    public Task<IEnumerable<GroupDTO>> Handle(GetGroupsByUserQuery request, CancellationToken cancellationToken)
+    {
+        IUserAccount user = _userQueryRepository.GetUserById(request.UserId)?.Account
+            ?? throw new UserNotFoundException($"No user found with id {request.UserId}");
+
+        return Task.FromResult(_groupQueryRepository
+            .GetGroupsByUser(user)
+            .Select(group => new GroupDTO(
+                group.GroupId,
+                group.GroupName,
+                group.GroupDescription,
+                group.Users
+                    .Select(user => user.Id)
+                    .ToList())));
+    }
+}
