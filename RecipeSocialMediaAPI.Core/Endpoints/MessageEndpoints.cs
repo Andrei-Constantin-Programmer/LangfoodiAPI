2cfd0914084731f12c81b4654664ff1b1af4e00d
nlg25 <nlg25@kent.ac.uk>
2024-01-28 16:10:30 +0000

    created MessageEndpoints.cs and the class MessageEndpoints

---
+++ RecipeSocialMediaAPI.Core/Endpoints/MessageEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+namespace RecipeSocialMediaAPI.Core.Endpoints;
+
+public static class MessageEndpoints
+{
+    public static void MapMessageEndpoints(this WebApplication app)
+    {
+        app.MapGroup("/message")
+            .AddMessageEndpoints()
+            .WithTags("Message");
+    }
+
+    private static RouteGroupBuilder AddMessageEndpoints(this RouteGroupBuilder group)
+    {
+
+        return group;
+    }
+}
