15a34749922b41483dc07f8d3e004d9a62c789e4
ff235 <ff235@kent.ac.uk>
2024-02-02 19:25:32 +0000

    Implement Handle_WhenGroupIdIsNotFound_ThrowArgumentException and import libraries

--- RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateGroupConversationHandlerTests.cs
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateGroupConversationHandlerTests.cs
@@ -1,10 +1,12 @@
 ﻿using FluentAssertions;
 using Moq;
 using RecipeSocialMediaAPI.Application.Contracts.Messages;
+using RecipeSocialMediaAPI.Application.Exceptions;
 using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
 using RecipeSocialMediaAPI.Application.Repositories.Messages;
 using RecipeSocialMediaAPI.Domain.Models.Messaging;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Conversations;
+using RecipeSocialMediaAPI.Domain.Tests.Shared;
 using RecipeSocialMediaAPI.TestInfrastructure;
 
 namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Commands;
@@ -59,15 +61,50 @@ public class CreateGroupConversationHandlerTests
         result.LastMessage.Should().BeNull();
     }
 
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.APPLICATION)]
     public async Task Handle_WhenGroupIdIsNotFound_ThrowArgumentException()
     {
         // Given
+        TestUserAccount userAccount1 = new()
+        {
+            Id = "user1",
+            Handler = "user1",
+            UserName = "UserName 1",
+            AccountCreationDate = new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero)
+        };
+        TestUserAccount userAccount2 = new()
+        {
+            Id = "user2",
+            Handler = "user2",
+            UserName = "UserName 2",
+            AccountCreationDate = new(2023, 2, 2, 0, 0, 0, TimeSpan.Zero)
+        };
 
+        Group group = new(
+            groupId: "group1",
+            groupName: "testGroup",
+            groupDescription: "This is a test group."
+        );
+
+        _groupQueryRepositoryMock
+            .Setup(repo => repo.GetGroupById(group.GroupId))
+            .Returns(group);
+
+        GroupConversation expectedConversation = new(group, "conversation1");
+
+        _groupConversationPersistenceRepositoryMock
+            .Setup(repo => repo.CreateGroupConversation(group))
+            .Returns(expectedConversation);
+
+        NewConversationContract testContract = new("invalidId");
 
         // When
-
+        var testAction = async () => await _groupConversationHandlerSUT.Handle(new CreateGroupConversationCommand(testContract), CancellationToken.None);
 
         // Then
-
+        await testAction
+            .Should().ThrowAsync<GroupNotFoundException>();
     }
 }
