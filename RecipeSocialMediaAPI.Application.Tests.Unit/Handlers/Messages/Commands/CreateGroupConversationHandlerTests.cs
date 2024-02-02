ac79b06a3351881a7ba8e31397d7b09aa5586a45
ff235 <ff235@kent.ac.uk>
2024-02-02 19:20:04 +0000

    Implement Handle_WhenGroupIdMatches_CreateAndReturnConnectionConversation

--- RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateGroupConversationHandlerTests.cs
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateGroupConversationHandlerTests.cs
@@ -19,16 +19,38 @@ public class CreateGroupConversationHandlerTests
         _groupConversationHandlerSUT = new(_groupConversationPersistenceRepositoryMock.Object, _groupQueryRepositoryMock.Object);
     }
 
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.APPLICATION)]
     public async Task Handle_WhenGroupIdMatches_CreateAndReturnConnectionConversation()
     {
         // Given
+        Group group = new(
+            groupId: "group1",
+            groupName: "testGroup",
+            groupDescription: "This is a test group."
+        );
+
+        _groupQueryRepositoryMock
+            .Setup(repo => repo.GetGroupById(group.GroupId))
+            .Returns(group);
         
+        GroupConversation expectedGroup = new(group, "conversation1");
+
+        _groupConversationPersistenceRepositoryMock
+            .Setup(repo => repo.CreateGroupConversation(group))
+            .Returns(expectedGroup);
+
+        NewConversationContract testContract = new(group.GroupId);
 
         // When
 
 
         // Then
-
+        result.Should().NotBeNull();
+        result.ConversationId.Should().Be(expectedGroup.ConversationId);
+        result.GroupId.Should().Be(group.GroupId);
+        result.LastMessage.Should().BeNull();
     }
 
     public async Task Handle_WhenGroupIdIsNotFound_ThrowArgumentException()
