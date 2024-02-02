c51690ed61150432703abbd766bd971653a572de
ff235 <ff235@kent.ac.uk>
2024-02-02 19:08:01 +0000

    Implement Handle_WhenConnectionIdIsNotFound_ThrowArgumentException

--- RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateConnectionConversationHandlerTests.cs
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/CreateConnectionConversationHandlerTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Moq;
 using RecipeSocialMediaAPI.Application.Contracts.Messages;
+using RecipeSocialMediaAPI.Application.Exceptions;
 using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
 using RecipeSocialMediaAPI.Application.Repositories.Messages;
 using RecipeSocialMediaAPI.Domain.Models.Messaging.Connections;
@@ -77,15 +78,51 @@ public class CreateConnectionConversationHandlerTests
         result.LastMessage.Should().BeNull();
     }
 
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.APPLICATION)]
     public async Task Handle_WhenConnectionIdIsNotFound_ThrowArgumentException()
     {
         // Given
+        TestUserAccount userAccount1 = new()
+        {
+            Id = "user1",
+            Handler = "user1",
+            UserName = "UserName 1",
+            AccountCreationDate = new(2023, 1, 1, 0, 0, 0, TimeSpan.Zero)
+        };
+        TestUserAccount userAccount2 = new()
+        {
+            Id = "user2",
+            Handler = "user2",
+            UserName = "UserName 2",
+            AccountCreationDate = new(2023, 2, 2, 0, 0, 0, TimeSpan.Zero)
+        };
 
+        Connection connection = new(
+            connectionId: "connection1",
+            account1: userAccount1,
+            account2: userAccount2,
+            status: ConnectionStatus.Connected
+        );
+
+        _connectionQueryRepositoryMock
+            .Setup(repo => repo.GetConnection(connection.ConnectionId))
+            .Returns(connection);
+
+        ConnectionConversation expectedConversation = new(connection, "conversation1");
+
+        _conversationPersistenceRepositoryMock
+            .Setup(repo => repo.CreateConnectionConversation(connection))
+            .Returns(expectedConversation);
+
+        NewConversationContract testContract = new("invalidId");
 
         // When
-
+        var testAction = async() => await _connectionConversationHandlerSUT.Handle(new CreateConnectionConversationCommand(testContract), CancellationToken.None);
 
         // Then
-
+        await testAction
+            .Should().ThrowAsync<ConnectionNotFoundException>();
     }
 }
