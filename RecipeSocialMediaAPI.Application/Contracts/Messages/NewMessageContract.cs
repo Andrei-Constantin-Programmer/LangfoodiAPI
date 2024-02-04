eb64af4d3fead2a8bf8f6ce1cc37f12e986060aa
ff235 <ff235@kent.ac.uk>
2024-02-04 13:51:30 +0000

    Added NewMessageContract

---
+++ RecipeSocialMediaAPI.Application/Contracts/Messages/NewMessageContract.cs
@@ -0,0 +1,2 @@
+﻿namespace RecipeSocialMediaAPI.Application.Contracts.Messages;
+public record NewMessageContract(string ConversationId, string SenderId, string? RecipientId, string Text, List<string> RecipeIds, List<string> ImageURLs, string SentDate, string? MessageRepliedTo);
