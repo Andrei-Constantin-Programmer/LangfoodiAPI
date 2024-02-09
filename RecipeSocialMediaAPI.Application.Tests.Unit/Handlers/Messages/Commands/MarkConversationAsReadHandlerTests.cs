691b45469146facb7148dc254d566f86ae8202a5
Andrei Constantin <andrei.constantin@outlook.com>
2024-02-09 21:09:12 +0000

    Created and initialised the MarkConversationAsRead command, handler, and test class

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Commands/MarkConversationAsReadHandlerTests.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Commands;
+
+public class MarkConversationAsReadHandlerTests
+{
+    private readonly Mock<IConversationQueryRepository> _conversationQueryRepositoryMock;
+    private readonly Mock<IMessagePersistenceRepository> _messagePersistenceRepositoryMock;
+
+    private readonly MarkConversationAsReadHandler _markConversationAsReadHandlerSUT;
+
+    public MarkConversationAsReadHandlerTests()
+    {
+        _conversationQueryRepositoryMock = new Mock<IConversationQueryRepository>();
+        _messagePersistenceRepositoryMock = new Mock<IMessagePersistenceRepository>();
+
+        _markConversationAsReadHandlerSUT = new(_conversationQueryRepositoryMock.Object, _messagePersistenceRepositoryMock.Object);
+    }
+}
---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Commands/MarkConversationAsReadCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.Repositories.Messages;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Commands;
+
+public record MarkConversationAsReadCommand(string UserId, string ConversationId) : IRequest;
+
+internal class MarkConversationAsReadHandler : IRequestHandler<MarkConversationAsReadCommand>
+{
+    private readonly IConversationQueryRepository _conversationQueryRepository;
+    private readonly IMessagePersistenceRepository _messagePersistenceRepository;
+
+    public MarkConversationAsReadHandler(IConversationQueryRepository conversationQueryRepository, IMessagePersistenceRepository messagePersistenceRepository)
+    {
+        _conversationQueryRepository = conversationQueryRepository;
+        _messagePersistenceRepository = messagePersistenceRepository;
+    }
+
+    public Task Handle(MarkConversationAsReadCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
