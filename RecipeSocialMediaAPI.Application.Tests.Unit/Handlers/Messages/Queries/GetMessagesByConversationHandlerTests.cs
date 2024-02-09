087caacafa18f12ff4247000049e09f13a782cea
Andrei Constantin <andrei.constantin@outlook.com>
2024-02-09 18:58:36 +0000

    Created and initialised GetMessagesByConversationQuery and its test class

---
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Messages/Queries/GetMessagesByConversationHandlerTests.cs
@@ -0,0 +1,13 @@
+﻿using RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Messages.Queries;
+
+public class GetMessagesByConversationHandlerTests
+{
+    private readonly GetMessagesByConversationHandler _getMessagesByConversationHandlerSUT;
+
+    public GetMessagesByConversationHandlerTests()
+    {
+        _getMessagesByConversationHandlerSUT = new();
+    }
+}
---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Queries/GetMessagesByConversationQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Queries;
+
+public record GetMessagesByConversationQuery(string ConversationId) : IRequest<List<MessageDTO>>;
+
+internal class GetMessagesByConversationHandler : IRequestHandler<GetMessagesByConversationQuery, List<MessageDTO>>
+{
+    public Task<List<MessageDTO>> Handle(GetMessagesByConversationQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
