05df482bd3aad8334b523f6f6229439ad8389d32
Andrei Constantin <andrei.constantin@outlook.com>
2024-02-15 20:11:32 +0000

    Wrote tests for MessagingHub's UpdateMessage(), DeleteMessage(), and MarkMessageAsRead() methods

--- RecipeSocialMediaAPI.Core.Tests.Unit/Hubs/MessagingHubTests.cs
+++ RecipeSocialMediaAPI.Core.Tests.Unit/Hubs/MessagingHubTests.cs
@@ -53,4 +53,64 @@ public class MessagingHubTests
         result.Should().Be(newMessage);
         _clientsMock.Verify(clients => clients.All.ReceiveMessage(newMessage), Times.Once);
     }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task UpdateMessage_NotifiesClientsOfMessageUpdate()
+    {
+        // Given
+        MessageDTO updatedMessage = new("m1", "u1", "User 1", new() { "u1" }, new(2024, 1, 1, 0, 0, 0, TimeSpan.Zero), TextContent: "Text");
+
+        UpdateMessageContract contract = new(updatedMessage.Id, updatedMessage.TextContent, new(), new());
+        _senderMock
+            .Setup(sender => sender.Send(It.Is<UpdateMessageCommand>(command => command.Contract == contract), CancellationToken.None))
+            .ReturnsAsync(updatedMessage);
+
+        // When
+        await _messagingHubSUT.UpdateMessage(contract);
+
+        // Then
+        _clientsMock.Verify(clients => clients.All.ReceiveMessageUpdate(updatedMessage), Times.Once);
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task DeleteMessage_NotifiesClientsOfMessageDeletion()
+    {
+        // Given
+        string messageId = "m1";
+
+        // When
+        await _messagingHubSUT.DeleteMessage(messageId);
+
+        // Then
+        _clientsMock.Verify(clients => clients.All.ReceiveMessageDeletion(messageId), Times.Once);
+        _senderMock
+            .Verify(sender => sender.Send(It.Is<RemoveMessageCommand>(command => command.Id == messageId), CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    [Trait(Traits.DOMAIN, Traits.Domains.MESSAGING)]
+    [Trait(Traits.MODULE, Traits.Modules.CORE)]
+    public async Task MarkMessageAsRead_NotifiesClientsOfTheMessageBeingRead()
+    {
+        // Given
+        string userId = "u1";
+        string messageId = "m1";
+
+        // When
+        await _messagingHubSUT.MarkMessageAsRead(userId, messageId);
+
+        // Then
+        _clientsMock.Verify(clients => clients.All.ReceiveMarkAsRead(userId, messageId), Times.Once);
+        _senderMock
+            .Verify(sender => 
+                sender.Send(It.Is<MarkMessageAsReadCommand>(command 
+                        => command.UserId == userId 
+                        && command.MessageId == messageId), 
+                    CancellationToken.None),
+                Times.Once);
+    }
 }
