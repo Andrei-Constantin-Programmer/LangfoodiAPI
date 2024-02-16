4932ef8b161f94824c93fa9251cd04e08e19c3a6
Andrei Constantin <andrei.constantin@outlook.com>
2024-02-16 17:17:24 +0000

    Implemented MessageCreatedNotification and its handler to notify users of a message having been created

---
+++ RecipeSocialMediaAPI.Application/Handlers/Messages/Notifications/MessageCreatedNotification.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Message;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Messages.Notifications;
+
+public record MessageCreatedNotification(MessageDTO Message, string ConversationId) : INotification;
+
+internal class MessageCreatedHandler : INotificationHandler<MessageCreatedNotification>
+{
+    private readonly IMessageNotificationService _messageNotificationService;
+
+    public MessageCreatedHandler(IMessageNotificationService messageNotificationService)
+    {
+        _messageNotificationService = messageNotificationService;
+    }
+
+    public async Task Handle(MessageCreatedNotification notification, CancellationToken cancellationToken)
+    {
+        await _messageNotificationService.NotifyMessageCreated(notification.Message, notification.ConversationId, cancellationToken);
+    }
+}
