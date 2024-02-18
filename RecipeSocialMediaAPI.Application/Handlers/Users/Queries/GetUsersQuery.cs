f0b3b7d3fc897b964b84219a6fe3f655b72a1d7a
Andrei Constantin <andrei.constantin@outlook.com>
2024-02-18 17:20:24 +0000

    Implemented the GetUsers query and handler

---
+++ RecipeSocialMediaAPI.Application/Handlers/Users/Queries/GetUsersQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using RecipeSocialMediaAPI.Application.DTO.Users;
+using RecipeSocialMediaAPI.Application.Mappers.Interfaces;
+using RecipeSocialMediaAPI.Application.Repositories.Users;
+
+namespace RecipeSocialMediaAPI.Application.Handlers.Users.Queries;
+
+public record GetUsersQuery(string ContainedString) : IRequest<List<UserAccountDTO>>;
+
+internal class GetUsersHandler : IRequestHandler<GetUsersQuery, List<UserAccountDTO>>
+{
+    private readonly IUserQueryRepository _userQueryRepository;
+    private readonly IUserMapper _userMapper;
+
+    public GetUsersHandler(IUserQueryRepository userQueryRepository, IUserMapper userMapper)
+    {
+        _userQueryRepository = userQueryRepository;
+        _userMapper = userMapper;
+    }
+
+    public async Task<List<UserAccountDTO>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        return await Task.FromResult(_userQueryRepository
+            .GetAllUserAccountsContaining(request.ContainedString)
+            .Select(_userMapper.MapUserAccountToUserAccountDto)
+            .ToList());
+    }
+}
