e27bcd846f390a80d0faf9fde9bf7bcb09c595a4
Andrei Constantin <andrei.constantin@outlook.com>
2024-03-08 00:27:59 +0000

    Added tests for the UnblockConnection handler

--- RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Users/Commands/UnblockConnectionHandlerTests.cs
+++ RecipeSocialMediaAPI.Application.Tests.Unit/Handlers/Users/Commands/UnblockConnectionHandlerTests.cs
@@ -11,21 +11,21 @@ using RecipeSocialMediaAPI.TestInfrastructure;
 
 namespace RecipeSocialMediaAPI.Application.Tests.Unit.Handlers.Users.Commands;
 
-public class BlockConnectionHandlerTests
+public class UnblockConnectionHandlerTests
 {
     private readonly Mock<IUserQueryRepository> _userQueryRepositoryMock;
     private readonly Mock<IUserPersistenceRepository> _userPersistenceRepositoryMock;
     private readonly Mock<IConnectionQueryRepository> _connectionQueryRepositoryMock;
 
-    private readonly BlockConnectionHandler _blockConnectionHandlerSUT;
+    private readonly UnblockConnectionHandler _unblockConnectionHandlerSUT;
 
-    public BlockConnectionHandlerTests()
+    public UnblockConnectionHandlerTests()
     {
         _userQueryRepositoryMock = new Mock<IUserQueryRepository>();
         _userPersistenceRepositoryMock = new Mock<IUserPersistenceRepository>();
         _connectionQueryRepositoryMock = new Mock<IConnectionQueryRepository>();
 
-        _blockConnectionHandlerSUT = new(
+        _unblockConnectionHandlerSUT = new(
             _userQueryRepositoryMock.Object,
             _userPersistenceRepositoryMock.Object,
             _connectionQueryRepositoryMock.Object);
@@ -41,10 +41,10 @@ public class BlockConnectionHandlerTests
             .Setup(repo => repo.GetUserByIdAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
             .ReturnsAsync((IUserCredentials?)null);
 
-        BlockConnectionCommand command = new("nonExistantUserId", "conn1");
+        UnblockConnectionCommand command = new("nonExistantUserId", "conn1");
 
         // When
-        var testAction = async () => await _blockConnectionHandlerSUT.Handle(command, CancellationToken.None);
+        var testAction = async () => await _unblockConnectionHandlerSUT.Handle(command, CancellationToken.None);
 
         // Then
         await testAction.Should().ThrowAsync<UserNotFoundException>();
@@ -72,10 +72,10 @@ public class BlockConnectionHandlerTests
             .Setup(repo => repo.GetUserByIdAsync(user.Account.Id, It.IsAny<CancellationToken>()))
             .ReturnsAsync(user);
 
-        BlockConnectionCommand command = new(user.Account.Id, "conn1");
+        UnblockConnectionCommand command = new(user.Account.Id, "conn1");
 
         // When
-        var testAction = async () => await _blockConnectionHandlerSUT.Handle(command, CancellationToken.None);
+        var testAction = async () => await _unblockConnectionHandlerSUT.Handle(command, CancellationToken.None);
 
         // Then
         await testAction.Should().ThrowAsync<ConnectionNotFoundException>();
@@ -84,7 +84,7 @@ public class BlockConnectionHandlerTests
     [Fact]
     [Trait(Traits.DOMAIN, Traits.Domains.USER)]
     [Trait(Traits.DOMAIN, Traits.Modules.APPLICATION)]
-    public async Task Handle_WhenBlockIsSuccessful_UpdateUser()
+    public async Task Handle_WhenUnblockIsSuccessful_UpdateUser()
     {
         // Given
         TestUserCredentials user1 = new()
@@ -109,21 +109,36 @@ public class BlockConnectionHandlerTests
             Email = "user2@mail.com",
             Password = "Pass@123"
         };
+        TestUserCredentials user3 = new()
+        {
+            Account = new TestUserAccount()
+            {
+                Id = "u3",
+                UserName = "User 3",
+                Handler = "user_3"
+            },
+            Email = "user3@mail.com",
+            Password = "Pass@123"
+        };
 
         _userQueryRepositoryMock
             .Setup(repo => repo.GetUserByIdAsync(user1.Account.Id, It.IsAny<CancellationToken>()))
             .ReturnsAsync(user1);
 
         Connection connection = new("conn1", user1.Account, user2.Account, ConnectionStatus.Connected);
-
         _connectionQueryRepositoryMock
             .Setup(repo => repo.GetConnectionAsync(connection.ConnectionId, It.IsAny<CancellationToken>()))
             .ReturnsAsync(connection);
 
-        BlockConnectionCommand command = new(user1.Account.Id, connection.ConnectionId);
+        Connection otherConnection = new("conn2", user1.Account, user3.Account, ConnectionStatus.Connected);
+
+        UnblockConnectionCommand command = new(user1.Account.Id, connection.ConnectionId);
+
+        user1.Account.BlockConnection(connection.ConnectionId);
+        user1.Account.BlockConnection(otherConnection.ConnectionId);
 
         // When
-        await _blockConnectionHandlerSUT.Handle(command, CancellationToken.None);
+        await _unblockConnectionHandlerSUT.Handle(command, CancellationToken.None);
 
         // Then
         user1.Account.Id.Should().Be("u1");
@@ -131,7 +146,8 @@ public class BlockConnectionHandlerTests
         user1.Account.Handler.Should().Be("user_1");
         user1.Email.Should().Be("user1@mail.com");
         user1.Password.Should().Be("Pass@123");
-        user1.Account.BlockedConnectionIds.Should().Contain(connection.ConnectionId);
+        user1.Account.BlockedConnectionIds.Should().NotContain(connection.ConnectionId);
+        user1.Account.BlockedConnectionIds.Should().Contain(otherConnection.ConnectionId);
         _userPersistenceRepositoryMock
             .Verify(repo => repo.UpdateUserAsync(user1, It.IsAny<CancellationToken>()), Times.Once);
     }
@@ -139,7 +155,7 @@ public class BlockConnectionHandlerTests
     [Fact]
     [Trait(Traits.DOMAIN, Traits.Domains.USER)]
     [Trait(Traits.DOMAIN, Traits.Modules.APPLICATION)]
-    public async Task Handle_WhenBlockIsUnsuccessful_DoNotUpdateUser()
+    public async Task Handle_WhenUnblockIsUnsuccessful_DoNotUpdateUser()
     {
         // Given
         TestUserCredentials user1 = new()
@@ -164,6 +180,17 @@ public class BlockConnectionHandlerTests
             Email = "user2@mail.com",
             Password = "Pass@123"
         };
+        TestUserCredentials user3 = new()
+        {
+            Account = new TestUserAccount()
+            {
+                Id = "u3",
+                UserName = "User 3",
+                Handler = "user_3"
+            },
+            Email = "user3@mail.com",
+            Password = "Pass@123"
+        };
 
         _userQueryRepositoryMock
             .Setup(repo => repo.GetUserByIdAsync(user1.Account.Id, It.IsAny<CancellationToken>()))
@@ -174,12 +201,14 @@ public class BlockConnectionHandlerTests
             .Setup(repo => repo.GetConnectionAsync(connection.ConnectionId, It.IsAny<CancellationToken>()))
             .ReturnsAsync(connection);
 
-        user1.Account.BlockConnection(connection.ConnectionId);
+        Connection otherConnection = new("conn2", user1.Account, user3.Account, ConnectionStatus.Connected);
 
-        BlockConnectionCommand command = new(user1.Account.Id, connection.ConnectionId);
+        UnblockConnectionCommand command = new(user1.Account.Id, connection.ConnectionId);
+
+        user1.Account.BlockConnection(otherConnection.ConnectionId);
 
         // When
-        await _blockConnectionHandlerSUT.Handle(command, CancellationToken.None);
+        await _unblockConnectionHandlerSUT.Handle(command, CancellationToken.None);
 
         // Then
         user1.Account.Id.Should().Be("u1");
@@ -187,7 +216,8 @@ public class BlockConnectionHandlerTests
         user1.Account.Handler.Should().Be("user_1");
         user1.Email.Should().Be("user1@mail.com");
         user1.Password.Should().Be("Pass@123");
-        user1.Account.BlockedConnectionIds.Should().Contain(connection.ConnectionId);
+        user1.Account.BlockedConnectionIds.Should().NotContain(connection.ConnectionId);
+        user1.Account.BlockedConnectionIds.Should().Contain(otherConnection.ConnectionId);
         _userPersistenceRepositoryMock
             .Verify(repo => repo.UpdateUserAsync(user1, It.IsAny<CancellationToken>()), Times.Never);
     }
